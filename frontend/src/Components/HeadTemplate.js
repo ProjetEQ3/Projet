64c393f907102531efe22151627161f1c9a2077f
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-09-06 17:50:36 -0400

    Head clean

--- frontend/src/Components/HeadTemplate.js
+++ frontend/src/Components/HeadTemplate.js
@@ -3,14 +3,15 @@ import React from "react";
 function HeadTemplate() {
     return (
         <header className="App-header">
-            <div className="container">
+            <div className="container-fluid">
                 <div className="row">
                     <div className="d-flex align-items-center justify-content-between">
-                        <p className="display-3">GlucOSE</p>
+                        <p className="display-3 mx-5 my-3">GlucOSE</p>
                         <p className="lead">Gestion Libre, Utile et Collaborative de l'Obtention de Stage Étudiant.</p>
-                        <div className="btn-group">
-                            <a className="text-ose btn">Connexion</a>
-                            <button className="text-ose btn">Inscription</button>
+                        <div className="align-self-start d-flex m-3">
+                            <a className="text-light text-decoration-none m-2 h6" href="/connexion">Connexion</a>
+                            <div className="vr"></div>
+                            <a className="text-light text-decoration-none m-2 h6" href="/inscription">Inscription</a>
                         </div>
                     </div>
                 </div>
