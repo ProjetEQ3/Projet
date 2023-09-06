b4db8cb056f58da1df058ff2fb5a4bc7d658a0ff
2055619 <2055619@claurendeau.qc.ca>
2023-09-06 11:51:56 -0400

    Initiation du projet

---
+++ glucose/src/main/java/cal/projeteq3/glucose/GlucoseApplication.java
@@ -0,0 +1,13 @@
+package cal.projeteq3.glucose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlucoseApplication.class, args);
+	}
+
+}
---
+++ glucose/src/test/java/cal/projeteq3/glucose/GlucoseApplicationTests.java
@@ -0,0 +1,13 @@
+package cal.projeteq3.glucose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlucoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
