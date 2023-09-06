7792c1967a794abd8e9ff94d17b7c3b2fe91c641
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-09-06 17:25:26 -0400

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import RegistrationForm from "./Components/RegistrationForm";
+import HeadTemplate from "./Components/HeadTemplate";
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm/>
+        <HeadTemplate/>
     </div>
   );
 }
