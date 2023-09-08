35228c4b450266cb92a46fb15ca47024059e6068
Zakaria <1@zaka.se>
2023-09-08 08:25:20 -0400

    Validation Messages en Fr

--- glucose/src/main/java/cal/projeteq3/glucose/validation/ValidationMessage.java
+++ glucose/src/main/java/cal/projeteq3/glucose/validation/ValidationMessage.java
@@ -1,17 +1,17 @@
 package cal.projeteq3.glucose.validation;
 
 public enum ValidationMessage{
-	EMAIL_MESSAGE("Email must be a valid email address."),
-	PASSWORD_MESSAGE("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number."),
-	NAME_MESSAGE("Name must be between 2 and 100 characters long and contain only letters, hyphens and apostrophes."),
-	AGE_MESSAGE("Age must be between 18 and 100 years old."),
-	STREET_MESSAGE("Street must be between 2 and 100 characters long and contain only letters, numbers, hyphens, apostrophes, and spaces."),
-	CITY_MESSAGE("City must be between 2 and 50 characters long and contain only letters, hyphens, and spaces."),
-	PROVINCE_MESSAGE("Province must be 2 characters long and contain only letters."),
-	POSTAL_CODE_MESSAGE("Postal code must be a valid postal code. (e.g. A1A 1A1)"),
-	COUNTRY_MESSAGE("Country must be between 2 and 50 characters long and contain only letters, hyphens, and spaces."),
-	PHONE_NUMBER_MESSAGE("Phone number must be a valid phone number."),
-	WEBSITE_MESSAGE("Website must be a valid URL.");
+	EMAIL_MESSAGE("L'adresse e-mail doit être une adresse e-mail valide."),
+	PASSWORD_MESSAGE("Le mot de passe doit contenir au moins 8 caractères et inclure au moins une lettre majuscule, une lettre minuscule et un chiffre."),
+	NAME_MESSAGE("Le nom doit contenir entre 2 et 100 caractères et ne peut contenir que des lettres, des tirets et des apostrophes."),
+	AGE_MESSAGE("L'âge doit être compris entre 18 et 100 ans."),
+	STREET_MESSAGE("La rue doit contenir entre 2 et 100 caractères et ne peut contenir que des lettres, des chiffres, des tirets, des apostrophes et des espaces."),
+	CITY_MESSAGE("La ville doit contenir entre 2 et 50 caractères et ne peut contenir que des lettres, des tirets et des espaces."),
+	PROVINCE_MESSAGE("La province doit contenir 2 caractères et ne peut contenir que des lettres."),
+	POSTAL_CODE_MESSAGE("Le code postal doit être un code postal valide. (par exemple, A1A 1A1)"),
+	COUNTRY_MESSAGE("Le pays doit contenir entre 2 et 50 caractères et ne peut contenir que des lettres, des tirets et des espaces."),
+	PHONE_NUMBER_MESSAGE("Le numéro de téléphone doit être un numéro de téléphone valide."),
+	WEBSITE_MESSAGE("Le site web doit être une URL valide.");
 
 	private final String string;
 
