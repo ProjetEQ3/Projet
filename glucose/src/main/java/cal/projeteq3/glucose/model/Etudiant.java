727cd17a0ecead418079cce169974439f9fe4097
2055619 <2055619@claurendeau.qc.ca>
2023-09-08 16:29:39 -0400

    Etudiant prend une string pour le département

--- glucose/src/main/java/cal/projeteq3/glucose/model/Etudiant.java
+++ glucose/src/main/java/cal/projeteq3/glucose/model/Etudiant.java
@@ -1,16 +1,22 @@
 package cal.projeteq3.glucose.model;
 
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Data
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Etudiant extends Utilisateur{
 
     private String matricule;
     private Departement departement;
+
+    public Etudiant(String nom, String prenom, String email, String password, String matricule, String departement) {
+        super(nom, prenom, email, password);
+        this.matricule = matricule;
+        this.departement = Departement.valueOf(departement);
+    }
 }
--- glucose/src/main/java/cal/projeteq3/glucose/model/Utilisateur.java
+++ glucose/src/main/java/cal/projeteq3/glucose/model/Utilisateur.java
@@ -4,10 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @MappedSuperclass
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class Utilisateur {
 
     @Id
@@ -18,4 +22,11 @@ public abstract class Utilisateur {
     private String prenom;
     private String adresseCourriel;
     private String motDePasse;
+
+    public Utilisateur(String nom, String prenom, String adresseCourriel, String motDePasse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresseCourriel = adresseCourriel;
+        this.motDePasse = motDePasse;
+    }
 }
