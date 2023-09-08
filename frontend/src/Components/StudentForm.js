6d4cb32b6007d50445e6300db5dc19d838085126
2164584 <2164584@claurendeau.qc.ca>
2023-09-08 10:55:16 -0400

    maj forms

--- frontend/src/Components/StudentForm.js
+++ frontend/src/Components/StudentForm.js
@@ -21,14 +21,14 @@ const StudentForm = () => {
     const [programmeValid, setProgrammeValid] = useState(true);
 
     const registerEtudiant = async () => {
-        const res = await axiosInstance.post('/etudiant/register',
+        const res = await axiosInstance.post('http://localhost:8080/etudiant/register',
             {
-                nom: this.nom,
-                prenom: this.prenom,
-                adresseCourriel: this.email,
-                motDePasse: this.password,
-                matricule: this.matricule,
-                departement: this.programme,
+                nom: formData.nom,
+                prenom: formData.prenom,
+                adresseCourriel: formData.email,
+                motDePasse: formData.password,
+                matricule: formData.matricule,
+                departement: formData.programme,
             }
         )
         return res.data
@@ -105,16 +105,16 @@ const StudentForm = () => {
             <div className="col-9 mx-auto">
                 <form onSubmit={handleSubmit}>
                     <div className="form-group">
-                        <label htmlFor="nom" className="mt-3">Nom</label>
-                        <input type="text" className="form-control" id="nom" placeholder="Nom" name="nom" onChange={handleChanges}/>
                         <label htmlFor="prenom" className="mt-3">Prénom</label>
                         <input type="text" className={`form-control ${prenomValid? '': 'is-invalid'}`} id="prenom" placeholder="Prénom" name="prenom" onChange={handleChanges}/>
+                        <label htmlFor="nom" className="mt-3">Nom</label>
+                        <input type="text" className={`form-control ${nomValid? '': 'is-invalid'}`} id="nom" placeholder="Nom" name="nom" onChange={handleChanges}/>
                         <label htmlFor="email" className="mt-3">Email</label>
-                        <input type="email" className="form-control" id="email" placeholder="Email" name="email" onChange={handleChanges}/>
+                        <input type="email" className={`form-control ${emailValid? '': 'is-invalid'}`} id="email" placeholder="Email" name="email" onChange={handleChanges}/>
                         <label htmlFor="numEtudiant" className="mt-3">Matricule du Cégep</label>
-                        <input type="text" className="form-control" id="matricule" placeholder="Matricule du Cégep" name="matricule" onChange={handleChanges}/>
+                        <input type="text" className={`form-control ${matriculeValid? '': 'is-invalid'}`} id="matricule" placeholder="Matricule du Cégep" name="matricule" onChange={handleChanges}/>
                         <label htmlFor="programme" className="mt-3">Programme d'étude</label>
-                        <select className="form-select" id="programme" onChange={handleChanges} name="programme" defaultValue="Choisir un programme">
+                        <select className={`form-control ${programmeValid? '': 'is-invalid'}`} id="programme" onChange={handleChanges} name="programme" defaultValue="Choisir un programme">
                             <option>Techniques de comptabilité et de gestion</option>
                             <option>Techniques de génie mécanique</option>
                             <option>Techniques de l’informatique</option>
@@ -132,9 +132,9 @@ const StudentForm = () => {
                             <option disabled={true}>Choisir un programme</option>
                         </select>
                         <label htmlFor="password" className="mt-3">Mot de passe</label>
-                        <input type="password" className="form-control" id="password" placeholder="Mot de passe" name="password" onChange={handleChanges}/>
+                        <input type="password" className={`form-control ${passwordValid? '': 'is-invalid'}`} id="password" placeholder="Mot de passe" name="password" onChange={handleChanges}/>
                         <label htmlFor="password" className="mt-3">Confirmer le mot de passe</label>
-                        <input type="password" className="form-control" id="password" placeholder="Mot de passe" name="passwordConfirm" onChange={handleChanges}/>
+                        <input type="password" className={`form-control ${passwordConfirmValid? '': 'is-invalid'}`} id="password" placeholder="Mot de passe" name="passwordConfirm" onChange={handleChanges}/>
 
                     </div>
                     <div className="row my-4">
