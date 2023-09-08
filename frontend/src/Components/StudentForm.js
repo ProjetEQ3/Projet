d8bf9f42ece9085e05dc19b9afd9b9cf79e0b252
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-09-08 08:53:11 -0400

    Modification ordre des inputsÃ

--- frontend/src/Components/StudentForm.js
+++ frontend/src/Components/StudentForm.js
@@ -124,13 +124,8 @@ const StudentForm = () => {
                         <input type="text" className="form-control" id="matricule" placeholder="Matricule du Cégep" name="matricule" onChange={handleChanges}/>
                         <label htmlFor="email" className="mt-3">Email</label>
                         <input type="email" className="form-control" id="email" placeholder="Email" name="email" onChange={handleChanges}/>
-                        <label htmlFor="password" className="mt-3">Mot de passe</label>
-                        <input type="password" className="form-control" id="password" placeholder="Mot de passe" name="password" onChange={handleChanges}/>
-                        <label htmlFor="password" className="mt-3">Confirmer le mot de passe</label>
-                        <input type="password" className="form-control" id="password" placeholder="Mot de passe" name="passwordConfirm" onChange={handleChanges}/>
                         <label htmlFor="programme" className="mt-3">Programme d'étude</label>
                         <select className="form-control" id="programme" onChange={handleChanges} name="programme" defaultValue="Choisir un programme">
-
                             <option>Techniques de comptabilité et de gestion</option>
                             <option>Techniques de génie mécanique</option>
                             <option>Techniques de l’informatique</option>
@@ -147,6 +142,11 @@ const StudentForm = () => {
                             <option>Technologie du génie civil</option>
                             <option disabled={true}>Choisir un programme</option>
                         </select>
+                        <label htmlFor="password" className="mt-3">Mot de passe</label>
+                        <input type="password" className="form-control" id="password" placeholder="Mot de passe" name="password" onChange={handleChanges}/>
+                        <label htmlFor="password" className="mt-3">Confirmer le mot de passe</label>
+                        <input type="password" className="form-control" id="password" placeholder="Mot de passe" name="passwordConfirm" onChange={handleChanges}/>
+
                     </div>
                     <div className="row my-4">
                         <div className="col-4 mx-auto">
