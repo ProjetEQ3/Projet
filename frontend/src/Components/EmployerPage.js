6eae8e43149abcb06ea6dac5a3ff0bfaf0aa3d1b
2164584 <2164584@claurendeau.qc.ca>
2023-09-11 11:38:24 -0400

    debut front end

--- frontend/src/Components/EmployerPage.js
+++ frontend/src/Components/EmployerPage.js
@@ -1,10 +1,27 @@
 import HeadTemplate from "./HeadTemplate";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const EmployerPage = () => {
+    const user = "foo";
     return (
         <div>
             <HeadTemplate/>
-            <h1>Employer Page</h1>
+            <div className="container">
+                <div className="row text-center">
+                    <div className="col-12">
+                        <h2 className="text-dark fw-light">Bonjour {user}!</h2>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12">
+                        <h3 className="text-dark fw-light">Vos offres de stages</h3>
+                        <div className="row">
+                            <button className="btn btn-outline-ose"><FontAwesomeIcon icon={faPlus} /></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
