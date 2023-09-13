66127cf5eed0dbbafd89ba918c2251d9d28fef5b
Zakaria <1@zaka.se>
2023-09-13 15:38:02 -0400

    layout

--- frontend/src/Components/layout/Main.jsx
+++ frontend/src/Components/layout/Main.jsx
@@ -1,8 +1,37 @@
+import {Route, Routes} from "react-router-dom";
+import Home from "../page/Home";
+import AuthPage from "../page/AuthPage";
+import StudentPage from "../page/StudentPage";
+import ManagerPage from "../page/ManagerPage";
+import EmployerPage from "../employer/EmployerPage";
+
 const Main = () => {
     return (
-        <div>
-            <h1>Main</h1>
-        </div>
+        <main className='App-main'>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<Home/>}
+                />
+                <Route
+                    path="auth/*"
+                    element={<AuthPage/>}
+                />
+                <Route
+                    path="etudiant/*"
+                    element={<StudentPage/>}
+                />
+                <Route
+                    path="manager/*"
+                    element={<ManagerPage/>}
+                />
+                <Route
+                    path="clientManager"
+                    element={<EmployerPage/>}
+                />
+                {/*<Route path="*" element={<PageNotFound/>}/>*/}
+            </Routes>
+        </main>
     )
 }
 
