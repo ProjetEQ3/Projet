6d601c0a80426a1247a188c6029ccb83debd29b7
2164584 <2164584@claurendeau.qc.ca>
2023-09-13 15:04:49 -0400

    before refactor

--- frontend/src/Components/ShortJobOffer.jsx
+++ frontend/src/Components/ShortJobOffer.jsx
@@ -1,7 +1,6 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import {useState} from "react";
-
 const ShortJobOffer = ({ jobOffer }) => {
     const [isHovered, setIsHovered] = useState(false);
 
@@ -31,7 +30,22 @@ const ShortJobOffer = ({ jobOffer }) => {
                                 {jobOffer.state}
                             </div>
                         </div>
-                        <FontAwesomeIcon icon={faTrash} className="trash-btn me-2 fa-lg"/>
+                        <button type="button" className="btn" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                            <FontAwesomeIcon icon={faTrash} className="me-2 fa-lg"/>
+                        </button>
+                        <div id="deleteModal" className="modal">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h3 className="modal-title">Êtes vous sûr de vouloir supprimer l'offre de stage ci-dessous:</h3>
+                                    </div>
+                                    <div className="modal-body">
+                                        <h4 className="text-dark fw-light pt-1">{jobOffer.title}</h4>
+                                        <p className="text-dark fw-light mb-3">{jobOffer.department}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
