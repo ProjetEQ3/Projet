f95412b8435e673b8440624106bc297d2d929fc7
SamuelGignac <samuelgignacwork@gmail.com>
2023-09-18 08:26:20 -0400

    JobOfferRepo function fix

--- src/main/java/cal/projeteq3/glucose/repository/JobOfferRepository.java
+++ src/main/java/cal/projeteq3/glucose/repository/JobOfferRepository.java
@@ -11,23 +11,10 @@ import java.util.List;
 @Repository
 public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {
 
-//    @Query("SELECT j FROM JobOffer j WHERE j.employer = ?1")
-//    List<JobOffer> findAllByEmployer(Long employerId);
-
-//  Pourquoi @Query ? Si JPA propose automatiquement une méthode
     List<JobOffer> findJobOfferByEmployer_Id(Long employerId);
-    List<JobOffer> findJobOfferByEmployer(Employer employer);
 
-//    @Query("SELECT j FROM JobOffer j WHERE j.jobOfferState = 'SUBMITTED'")
-//    List<JobOffer> findSubmittedJobOffers();
-//
-//    @Query("SELECT j FROM JobOffer j WHERE j.jobOfferState = 'ACCEPTED'")
-//    List<JobOffer> findAcceptedJobOffers();
-//
-//    @Query("SELECT j FROM JobOffer j WHERE j.jobOfferState = 'REFUSED'")
-//    List<JobOffer> findRefusedJobOffers();
+    List<JobOffer> findJobOffersByEmployer(Employer employer);
 
-//    Pourquoi 3 méthodes différentes ? Sam vien  me parler (Louis) si t'as du trouble avec JPA
     List<JobOffer> findJobOfferByJobOfferState(JobOfferState jobOfferState);
 
 }
