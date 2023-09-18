48dc54ea85e4c14038c7a28c18180302f3f07b28
2164584 <2164584@claurendeau.qc.ca>
2023-09-18 11:25:03 -0400

    login css fait

--- frontend/src/Components/auth/LoginForm.jsx
+++ frontend/src/Components/auth/LoginForm.jsx
@@ -3,17 +3,21 @@ import {useNavigate} from "react-router-dom"
 import {toast} from "react-toastify"
 import {axiosInstance} from "../../App"
 import User from "../../model/User";
+import Loading from "../util/Loading";
 
 const LoginForm = ({user, setUser}) => {
 	const navigate = useNavigate()
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
+	const [isLoading, setIsLoading] = useState(false)
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	});
 
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		const data = {
-			email: email,
-			password: password
+			email: formData.email,
+			password: formData.password
 		}
 		console.log("data", data)
 		if(validateUser())
@@ -49,12 +53,17 @@ const LoginForm = ({user, setUser}) => {
 
 	const validateEmail = () => {
 		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-		return emailRegex.test(email)
+		return emailRegex.test(formData.email)
 	}
 
 	const validatePassword = () => {
 		const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-		return passwordRegex.test(password)
+		return passwordRegex.test(formData.password)
+	}
+
+	const handleChanges = (e) => {
+		const {name, value} = e.target
+		setFormData({...formData, [name]: value.trim()})
 	}
 
 	return (
@@ -68,36 +77,21 @@ const LoginForm = ({user, setUser}) => {
 				) : (
 					<>
 						<div className="container">
-							<div className="row justify-content-center align-items-center">
-								<form id="login-form" className="col-10">
-									<h1 className="display-6 text-center mb-5">GlucOSE</h1>
-									<div className="form-group row mx-auto">
-										<label htmlFor="colFormLabelSm" className="col-sm-3 col-form-label">Email</label>
-										<div className="col-12">
-											<input type="email" className="form-control" id="inputEmail1"
-												   aria-describedby="emailHelp" placeholder="Entrez email" value={email}
-												   onChange={(e) => setEmail(e.target.value)}
-												   required
-											/>
-										</div>
-									</div>
-									<div className="mt-2"></div>
-									<div className="form-group row mx-auto">
-										<label htmlFor="colFormLabelLg" className="col-sm-3 col-form-label">Password</label>
-										<div className="col-12">
-											<input
-												type="password" className="form-control" id="inputPassword"
-												placeholder="Mot de pass" value={password}
-												onChange={(e) => setPassword(e.target.value)}
-												required
-											/>
-										</div>
-									</div>
+							<h1 className="display-6 text-center mb-3">GlucOSE</h1>
+							{isLoading ? <Loading/> :
+							<div className="row">
+								<div className="col-9 mx-auto">
+								<form id="login-form" className="form-group">
+									<label htmlFor="colFormLabelSm" className="col-sm-3 mt-3">Email</label>
+									<input type="email" className="form-control" placeholder="Entrez email" name="email" onChange={handleChanges} required/>
+									<label htmlFor="colFormLabelLg" className="col-sm-3 mt-3">Password</label>
+									<input type="password" className="form-control" placeholder="Mot de pass" name="password" onChange={handleChanges} required/>
 									<div className="row col-6 mx-auto">
 										<button type="submit" className="btn btn-outline-ose my-5 mx-auto" onClick={handleSubmit}>Se connecter</button>
 									</div>
 								</form>
-							</div>
+								</div>
+							</div>}
 						</div>
 					</>
 				)
