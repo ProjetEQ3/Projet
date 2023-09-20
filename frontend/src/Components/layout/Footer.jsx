faa77a044ca49e487a8d36560ca01c6207f42a61
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-09-20 08:33:37 -0400

    Un peu de responsive, ça fait pas de mal

--- frontend/src/Components/layout/Footer.jsx
+++ frontend/src/Components/layout/Footer.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer className="App-footer">
+        <footer className="App-footer position-absolute col-12 bottom-0">
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-12 text-center">
+                    <div className="col-12 d-none d-md-block text-center">
                         <p className="lead">GlucOSE - Gestion Libre, Utile et Collaborative de l'Obtention de Stage Étudiant.</p>
                         <p className="lead">© 2023 - Tous droits réservés.</p>
+                    </div>
+                    <div className="col-12 d-block d-md-none text-center">
+                        <p className="lead"><small>GlucOSE - Gestion Libre, Utile et Collaborative de l'Obtention de Stage Étudiant.</small></p>
+                        <p className="lead"><small>© 2023 - Tous droits réservés.</small></p>
                     </div>
                 </div>
             </div>
--- frontend/src/Components/layout/Header.jsx
+++ frontend/src/Components/layout/Header.jsx
@@ -10,11 +10,11 @@ function Header({user}) {
                     <div className="d-flex align-items-center justify-content-between">
                         <NavLink className="text-decoration-none text-white col-2" to="/">
                             <div className="d-flex">
-                                <img alt="Logo" className="col-4" src={logo}/>
+                                <img alt="Logo" className="col-4 d-none d-md-block" src={logo}/>
                                 <p className="display-3 ms-2 my-3">GlucOSE</p>
                             </div>
                         </NavLink>
-                        <p className="lead">Gestion Libre, Utile et Collaborative de l'Obtention de Stage Étudiant.</p>
+                        <p className="lead d-none d-md-block">Gestion Libre, Utile et Collaborative de l'Obtention de Stage Étudiant.</p>
                         {user?.isLoggedIn ?
                             <div className="m-3 text-center lh-1">
                                 <h2 className="display-6">{user.firstName + " " + user.lastName}</h2>
