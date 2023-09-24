48378a0237414883122eb39e5965fdc9458e3432
2055619 <2055619@claurendeau.qc.ca>
2023-09-24 12:34:48 -0400

    EQ3-112 Ajout Test de controller

--- src/test/java/cal/projeteq3/glucose/controller/StudentControllerTest.java
+++ src/test/java/cal/projeteq3/glucose/controller/StudentControllerTest.java
@@ -1,155 +1,159 @@
 package cal.projeteq3.glucose.controller;
 
+import cal.projeteq3.glucose.dto.JobOfferDTO;
+import cal.projeteq3.glucose.model.Department;
+import cal.projeteq3.glucose.model.jobOffer.JobOffer;
+import cal.projeteq3.glucose.model.jobOffer.JobOfferState;
+import cal.projeteq3.glucose.repository.JobOfferRepository;
+import cal.projeteq3.glucose.repository.StudentRepository;
 import cal.projeteq3.glucose.service.StudentService;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.mockito.Mockito.when;
+
+@SpringJUnitConfig(classes = {StudentController.class, StudentService.class})
+@WebMvcTest(StudentController.class)
 public class StudentControllerTest {
 
-    private StudentController studentController;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private StudentRepository studentRepository;
+
+    @Mock
+    private JobOfferRepository jobOfferRepository;
+
+    @MockBean
+//    @InjectMocks
     private StudentService studentService;
 
-    @BeforeEach
-    void setUp() {
-        studentService = mock(StudentService.class);
-        studentController = new StudentController(studentService);
+
+    @Test
+    public void testGetJobOffersByDepartment() throws Exception {
+//        Arrange
+        List<JobOffer> jobOffers_420B0 = new ArrayList<>(
+                List.of(
+                        JobOffer.builder()
+                                .title("JobOffer1")
+                                .description("Description1")
+                                .location("Location1")
+                                .department(Department._420B0)
+                                .jobOfferState(JobOfferState.OPEN)
+                                .duration(6)
+                                .hoursPerWeek(40)
+                                .salary(20.0f)
+                                .startDate(LocalDateTime.now())
+                                .expirationDate(LocalDateTime.now().plusDays(30))
+                                .build(),
+                        JobOffer.builder()
+                                .title("JobOffer2")
+                                .description("Description2")
+                                .location("Location1")
+                                .department(Department._420B0)
+                                .jobOfferState(JobOfferState.SUBMITTED)
+                                .duration(6)
+                                .hoursPerWeek(40)
+                                .salary(20.0f)
+                                .startDate(LocalDateTime.now())
+                                .expirationDate(LocalDateTime.now().plusDays(30))
+                                .build(),
+                        JobOffer.builder()
+                                .title("JobOffer3")
+                                .description("Description3")
+                                .location("Location1")
+                                .department(Department._420B0)
+                                .jobOfferState(JobOfferState.EXPIRED)
+                                .duration(6)
+                                .hoursPerWeek(40)
+                                .salary(20.0f)
+                                .startDate(LocalDateTime.now().minusDays(60))
+                                .expirationDate(LocalDateTime.now().minusDays(30))
+                                .build()
+                )
+        );
+        when(studentService.getJobOffersByDepartment(Department._420B0))
+                .thenReturn(jobOffers_420B0.stream().map(JobOfferDTO::new).collect(Collectors.toList()));
+
+
+//        Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/jobOffers/{department}", "_420B0"))
+                .andExpect(MockMvcResultMatchers.status().isAccepted())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("JobOffer1"));
     }
 
-    /*@Test
-    void Register_Valid() {
-        // Arrange
-        Student validStudent = new Student("Michel", "Michaud", "T@T.com", "Ose12asd3", "1234567", "_420B0", null);
-
-        when(studentService.createStudent(validStudent)).thenReturn(new StudentDTO("1234567", Department._420B0));
-
-        // Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(validStudent);
-
-        // Assert
-        assertEquals(HttpStatus.ACCEPTED, responseEntity.getStatusCode());
-        assertNotNull(responseEntity.getBody());
-    }*/
-
-    /*@Test
-    void Register_MissingFirstName(){
+    @Test
+    public void testGetOpenJobOffersByDepartment() throws Exception {
 //        Arrange
-        Student invalidStudent = new Student(
-                "",
-                "Michaud",
-                "T@T.com",
-                "Ose12asd3",
-                "1234567",
-                "_420B0",
-                null);
+        List<JobOffer> jobOffers_420B0 = new ArrayList<>(
+                List.of(
+                        JobOffer.builder()
+                                .title("JobOffer1")
+                                .description("Description1")
+                                .location("Location1")
+                                .department(Department._420B0)
+                                .jobOfferState(JobOfferState.OPEN)
+                                .duration(6)
+                                .hoursPerWeek(40)
+                                .salary(20.0f)
+                                .startDate(LocalDateTime.now())
+                                .expirationDate(LocalDateTime.now().plusDays(30))
+                                .build(),
+                        JobOffer.builder()
+                                .title("JobOffer2")
+                                .description("Description2")
+                                .location("Location1")
+                                .department(Department._420B0)
+                                .jobOfferState(JobOfferState.SUBMITTED)
+                                .duration(6)
+                                .hoursPerWeek(40)
+                                .salary(20.0f)
+                                .startDate(LocalDateTime.now())
+                                .expirationDate(LocalDateTime.now().plusDays(30))
+                                .build(),
+                        JobOffer.builder()
+                                .title("JobOffer3")
+                                .description("Description3")
+                                .location("Location1")
+                                .department(Department._420B0)
+                                .jobOfferState(JobOfferState.EXPIRED)
+                                .duration(6)
+                                .hoursPerWeek(40)
+                                .salary(20.0f)
+                                .startDate(LocalDateTime.now().minusDays(60))
+                                .expirationDate(LocalDateTime.now().minusDays(30))
+                                .build()
+                )
+        );
 
-//        Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(invalidStudent);
+        when(studentService.getOpenJobOffersByDepartment(Department._420B0))
+                .thenReturn(jobOffers_420B0.stream().map(JobOfferDTO::new).filter(jobOfferDTO -> {
+                    return jobOfferDTO.getJobOfferState().equals(JobOfferState.OPEN);
+                }).collect(Collectors.toList()));
 
-//        Assert
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-
-    }*/
-
-    /*@Test
-    void Register_MissingLastName(){
-//        Arrange
-        Student invalidStudent = new Student(
-                "Michel",
-                "",
-                "T@T.com",
-                "Ose12asd3",
-                "1234567",
-                "_420B0",
-                null);
-
-//        Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(invalidStudent);
-
-//        Assert
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-
-    }*/
-
-    /*@Test
-    void Register_MissingEmail(){
-//        Arrange
-        Student invalidStudent = new Student(
-                "Michel",
-                "Michaud",
-                "",
-                "Ose12asd3",
-                "1234567",
-                "_420B0",
-                null);
-
-//        Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(invalidStudent);
-
-//        Assert
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-
-    }*/
-
-    /*@Test
-    void Register_MissingPassword(){
-//        Arrange
-        Student invalidStudent = new Student(
-                "Michel",
-                "Michaud",
-                "T@T.com",
-                "",
-                "1234567",
-                "_420B0",
-                null);
-
-//        Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(invalidStudent);
-
-//        Assert
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-
-    }*/
-
-    /*@Test
-    void Register_MissingMatricule(){
-//        Arrange
-        Student invalidStudent = new Student(
-                "Michel",
-                "Michaud",
-                "T@T.com",
-                "Ose12asd3",
-                "",
-                "_420B0",
-                null);
-
-//        Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(invalidStudent);
-
-//        Assert
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-
-    }*/
-
-    /*@Test
-    void Register_MissingDepartement(){
-//        Arrange
-        Student invalidStudent = new Student(
-                "Michel",
-                "Michaud",
-                "T@T.com",
-                "Ose12asd3",
-                "1234567",
-                "_420A0",
-                null);
-
-//        Act
-        ResponseEntity<StudentDTO> responseEntity = studentController.register(invalidStudent);
-
-//        Assert
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
-
-    }*/
+//        Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/student/jobOffers/open/{department}", "_420B0"))
+                .andExpect(MockMvcResultMatchers.status().isAccepted())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].title").value("JobOffer1"));
+    }
 }
