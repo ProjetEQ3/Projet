795a418c09109fb1b7492df29f144d8c0dcd7e1c
2055619 <2055619@claurendeau.qc.ca>
2023-09-25 14:57:46 -0400

    Retrait des champs inutiles

--- frontend/src/Components/student/ShortJobOffer.jsx
+++ frontend/src/Components/student/ShortJobOffer.jsx
@@ -14,14 +14,9 @@ const ShortJobOffer = ({jobOffer}) => {
         <div className={`row ${!isHovered? 'm-2':'m-1 shadow'}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className="col-12 bg-white rounded">
                 <div className="row">
-                    <div className="col-8">
+                    <div className="col-12">
                         <h5 className="text-dark fw-light pt-1">{jobOffer.title}</h5>
                         <p className="text-dark fw-light mb-3">{jobOffer.department}</p>
-                    </div>
-                    <div className="col-4 my-auto d-flex justify-content-between">
-                        <div className="mx-auto">
-                            <p>Chose à mettre</p>
-                        </div>
                     </div>
                 </div>
             </div>
