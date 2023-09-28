f6a12569ba23fcd83e0286a9377dcd89c80c696d
Zakaria <1@zaka.se>
2023-09-27 23:42:28 -0400

    Filter Fix

--- frontend/src/Components/util/FilterObjectList.jsx
+++ frontend/src/Components/util/FilterObjectList.jsx
@@ -51,52 +51,69 @@ const FilterObjectList = ({items, attributes, renderItem, selectOptions}) => {
 	return (
 		<div className="mb-3">
 			<h5 className="mb-2">Filter by:</h5>
-			<div className="d-flex align-items-center mb-2 col-6">
-				<select className="form-select me-2 flex-grow-1" value={selectedAttribute} onChange={handleAttributeChange}>
-					{attributes.map(attr => (
-						<option key={attr} value={attr}>{getAttributeDisplayName(attr)}</option>
-					))}
-				</select>
-				{isSelectAttribute ? (
-					<select className="form-control me-2 flex-grow-1" value={query} onChange={handleInputChange}>
-						<option value="">Choose an option</option>
-						{selectOptions[actualAttribute].map(option => (
-							<option key={option} value={option}>{option}</option>
+
+			<div className="d-flex align-items-center justify-content-between mb-2 col-12">
+				<div className="d-flex align-items-center flex-grow-1 col-6">
+					<select className="form-select me-2 flex-grow-1" value={selectedAttribute} onChange={handleAttributeChange}>
+						{attributes.map(attr => (
+							<option key={attr} value={attr}>{getAttributeDisplayName(attr)}</option>
 						))}
 					</select>
-				) : (
-					<input
-						className="form-control me-2 flex-grow-1"
-						type="text"
-						placeholder={`Filter by ${getAttributeDisplayName(selectedAttribute)}...`}
-						value={query}
-						onChange={handleInputChange}
-					/>
-				)}
-				<select className="form-select" value={itemsPerPage} onChange={handleItemsPerPageChange}>
-					{[10, 20, 50, 100].map(number => (
-						<option key={number} value={number}>
-							{number} per page
-						</option>
-					))}
-				</select>
-			</div>
-			<div className="d-flex justify-content-between align-items-center mb-2 col-6">
-				<div className="pagination-controls">
-					<button className="btn btn-outline-secondary me-2" onClick={goToPreviousPage} disabled={currentPage === 1}>
-						Previous
-					</button>
-					<span className="me-2">Page {currentPage} of {totalPages}</span>
-					<button className="btn btn-outline-secondary" onClick={goToNextPage} disabled={currentPage === totalPages}>
-						Next
-					</button>
+
+					{isSelectAttribute ? (
+						<select className="form-control me-2 flex-grow-1" value={query} onChange={handleInputChange}>
+							<option value="">Choose an option</option>
+							{selectOptions[actualAttribute].map(option => (
+								<option key={option} value={option}>{option}</option>
+							))}
+						</select>
+					) : (
+						<input
+							className="form-control me-2 flex-grow-1"
+							type="text"
+							placeholder={`Filter by ${getAttributeDisplayName(selectedAttribute)}...`}
+							value={query}
+							onChange={handleInputChange}
+						/>
+					)}
+				</div>
+
+				{/*move to the right side*/}
+				<div className="d-flex align-items-center col-6">
+					<select className="form-select me-2" value={itemsPerPage} onChange={handleItemsPerPageChange}>
+						{[10, 20, 50, 100].map(number => (
+							<option key={number} value={number}>
+								{number} per page
+							</option>
+						))}
+					</select>
+
+					<div className="pagination-controls d-flex align-items-center">
+						<button className="btn btn-outline-secondary me-2" onClick={goToPreviousPage} disabled={currentPage === 1}>
+							Previous
+						</button>
+
+						<div className="d-flex align-items-center me-2">
+							<span className="me-1">Page</span>
+							<span>{currentPage}</span>
+							<span className="mx-1">of</span>
+							<span>{totalPages}</span>
+						</div>
+
+						<button className="btn btn-outline-secondary" onClick={goToNextPage} disabled={currentPage === totalPages}>
+							Next
+						</button>
+					</div>
+
 				</div>
 			</div>
+
 			<div className="mt-3">
 				{renderItem(displayedItems)}
 			</div>
 		</div>
 	)
+
 
 }
 
