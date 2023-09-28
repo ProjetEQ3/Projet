fb3f7fbfae73ed4ecaac66d55f21e04eefcb7d5f
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-09-27 23:59:45 -0400

    Modifs mineures sur le CSS

--- frontend/src/Components/util/FilterObjectList.jsx
+++ frontend/src/Components/util/FilterObjectList.jsx
@@ -50,16 +50,13 @@ const FilterObjectList = ({items, attributes, renderItem, selectOptions}) => {
 
 	return (
 		<div className="mb-3">
-			<h5 className="mb-2">Filter by:</h5>
-
 			<div className="d-flex align-items-center justify-content-between mb-2 col-12">
-				<div className="d-flex align-items-center flex-grow-1 col-6">
+				<div className="d-flex align-items-center col-6">
 					<select className="form-select me-2 flex-grow-1" value={selectedAttribute} onChange={handleAttributeChange}>
 						{attributes.map(attr => (
 							<option key={attr} value={attr}>{getAttributeDisplayName(attr)}</option>
 						))}
 					</select>
-
 					{isSelectAttribute ? (
 						<select className="form-control me-2 flex-grow-1" value={query} onChange={handleInputChange}>
 							<option value="">Choose an option</option>
@@ -77,44 +74,37 @@ const FilterObjectList = ({items, attributes, renderItem, selectOptions}) => {
 						/>
 					)}
 				</div>
-
-				{/*move to the right side*/}
-				<div className="d-flex align-items-center col-6">
+				<div className="d-flex align-items-center col-3">
 					<select className="form-select me-2" value={itemsPerPage} onChange={handleItemsPerPageChange}>
-						{[10, 20, 50, 100].map(number => (
+						{[5, 10, 20, 50, 100].map(number => (
 							<option key={number} value={number}>
 								{number} per page
 							</option>
 						))}
 					</select>
-
-					<div className="pagination-controls d-flex align-items-center">
-						<button className="btn btn-outline-secondary me-2" onClick={goToPreviousPage} disabled={currentPage === 1}>
-							Previous
-						</button>
-
-						<div className="d-flex align-items-center me-2">
-							<span className="me-1">Page</span>
-							<span>{currentPage}</span>
-							<span className="mx-1">of</span>
-							<span>{totalPages}</span>
-						</div>
-
-						<button className="btn btn-outline-secondary" onClick={goToNextPage} disabled={currentPage === totalPages}>
-							Next
-						</button>
-					</div>
-
 				</div>
 			</div>
-
-			<div className="mt-3">
+			<div className="my-3">
 				{renderItem(displayedItems)}
+			</div>
+			<div className="pagination-controls mx-auto col-12">
+				<div className="d-flex align-items-center justify-content-around">
+					<button className="btn btn-outline-ose me-2 col-md-2 col-3" onClick={goToPreviousPage} disabled={currentPage === 1}>
+						Previous
+					</button>
+					<div className="d-flex align-items-center me-2">
+						<span className="me-1">Page</span>
+						<span>{currentPage}</span>
+						<span className="mx-1">of</span>
+						<span>{totalPages}</span>
+					</div>
+					<button className="btn btn-outline-ose col-md-2 col-3" onClick={goToNextPage} disabled={currentPage === totalPages}>
+						Next
+					</button>
+				</div>
 			</div>
 		</div>
 	)
-
-
 }
 
 FilterObjectList.defaultProps = {
