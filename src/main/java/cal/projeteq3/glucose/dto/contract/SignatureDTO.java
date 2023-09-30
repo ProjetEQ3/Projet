79f9263051817944f328698a0a02138d05db0aa2
2055619 <2055619@claurendeau.qc.ca>
2023-09-29 20:34:44 -0400

    Signature DTO

---
+++ src/main/java/cal/projeteq3/glucose/dto/contract/SignatureDTO.java
@@ -0,0 +1,21 @@
+package cal.projeteq3.glucose.dto.contract;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SignatureDTO {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String jobTitle;
+    private LocalDate signatureDate;
+    private Long contractId;
+
+    // Constructors, getters, and setters as needed
+}
