2e4202e4b077af4f4230c3b875873c9ce0867586
2055619 <2055619@claurendeau.qc.ca>
2023-10-01 16:01:24 -0400

    EQ3-158 FullJobOffer.jsx

--- frontend/src/Components/manager/FullJobOffer.jsx
+++ frontend/src/Components/manager/FullJobOffer.jsx
@@ -4,8 +4,10 @@ import { faCheck, faX } from '@fortawesome/free-solid-svg-icons';
 import {axiosInstance} from "../../App";
 import {toast} from "react-toastify";
 import State from "../util/State";
+import {useTranslation} from "react-i18next";
 
 const FullJobOffer = ({ jobOffer, updateJobOfferList }) => {
+    const {t} = useTranslation();
     const [isDecline, setIsDecline] = useState(false);
     const [formData, setFormData] = useState({
         refusalReason: '',
@@ -14,11 +16,11 @@ const FullJobOffer = ({ jobOffer, updateJobOfferList }) => {
         e.preventDefault();
         axiosInstance.put(`/manager/jobOffer/accept/${jobOffer.id}`)
             .then((res) => {
-                toast.success("Offre acceptée")
+                toast.success(t('acceptedOffer'))
                 updateJobOfferList(jobOffer);
             })
             .catch((error) => {
-                toast.error("Impossible d'accepter l'offre: " + error.message)
+                toast.error(t('pushingError') + error.message)
             })
     }
 
@@ -44,17 +46,17 @@ const FullJobOffer = ({ jobOffer, updateJobOfferList }) => {
         if (document.getElementById('refusalForm').checkValidity() === false) {
             e.stopPropagation();
             document.getElementById('refusalForm').classList.add('was-validated');
-            toast.error("Please fill in the refusal reason")
+            toast.error(t('addRefusalReason'))
             return;
         }
 
         axiosInstance.put(`/manager/jobOffer/refuse/${jobOffer.id}`, formData.refusalReason)
             .then((res) => {
-                toast.success("Offer declined")
+                toast.success(t('refusedOffer'))
                 updateJobOfferList(jobOffer);
             })
             .catch((error) => {
-                toast.error("Failed to decline offer: " + error.message)
+                toast.error(t('pushingError') + error.message)
             })
         setIsDecline(false)
     }
@@ -68,7 +70,7 @@ const FullJobOffer = ({ jobOffer, updateJobOfferList }) => {
         <div className="modal-dialog">
             <div className="modal-content">
                 <div className="modal-header">
-                    <h3 className="modal-title">Autorisation de l'offre</h3>
+                    <h3 className="modal-title">{t('authorizeOffer')}</h3>
                     <FontAwesomeIcon icon={faX} data-bs-dismiss="modal" className="danger-hover fa-lg pe-2" onClick={handleClose}/>
                 </div>
                 <div className="modal-body">
@@ -79,26 +81,26 @@ const FullJobOffer = ({ jobOffer, updateJobOfferList }) => {
                     <p className="text-dark fw-light mb-3">{jobOffer.department}</p>
                     <p className="text-dark fw-light mb-3">{jobOffer.location}</p>
                     { jobOffer.startDate !== undefined &&
-                        (<p className="text-dark fw-light mb-3">Date de début: {jobOffer.startDate.split('T')[0]}</p>)
+                        (<p className="text-dark fw-light mb-3">{t('startDate') + jobOffer.startDate}</p>)
                     }
-                    <p className="text-dark fw-light mb-3">Durée: {jobOffer.duration} semaine(s)</p>
+                    <p className="text-dark fw-light mb-3">{t('duration') + jobOffer.duration + t('week')}</p>
                     { jobOffer.expirationDate !== undefined &&
-                        (<p className="text-dark fw-light mb-3">Date d'expiration: {jobOffer.expirationDate.split('T')[0]}</p>)
+                        (<p className="text-dark fw-light mb-3">Date d'expiration: {t('endDate') + jobOffer.expirationDate}</p>)
                     }
                     <p className="text-dark fw-light mb-3">{jobOffer.salary}$/h</p>
-                    <p className="text-dark fw-light mb-3">{jobOffer.hoursPerWeek}h/semaine</p>
+                    <p className="text-dark fw-light mb-3">{jobOffer.hoursPerWeek}h/{t('week')}</p>
                     <p className="text-dark fw-light mb-3">{jobOffer.description}</p>
                 </div>
                 <div className="modal-footer">
                     {isDecline ? (
                         <form id="refusalForm" className="form col-10 mx-auto">
                             <p>Êtes-vous sûr de vouloir refuser cette offre?</p>
-                            <input id="refusalReason" name="refusalReason" className="form-control form-text" type="text" onChange={validateReason} placeholder="Raison du refus" required/>
+                            <input id="refusalReason" name="refusalReason" className="form-control form-text" type="text" onChange={validateReason} placeholder={t('refusalReason')} required/>
                             <input value="Confirmer" type="submit" onClick={confirmDecline} className="btn btn-primary m-2" data-bs-dismiss="modal"/>
-                            <button type="button" onClick={cancelDecline} className="btn btn-outline-secondary ms-2" data-bs-dismiss="modal">Annuler</button>
+                            <button type="button" onClick={cancelDecline} className="btn btn-outline-secondary ms-2" data-bs-dismiss="modal">{t('cancel')}</button>
                         </form>) :
                         (<div>
-                            <button type="button" onClick={handleAccept} className="btn btn-success mx-2" data-bs-dismiss="modal">Accepter</button>
+                            <button type="button" onClick={handleAccept} className="btn btn-success mx-2" data-bs-dismiss="modal">{t('accept')}</button>
                             <button type="button" onClick={handleDecline} className="btn btn-danger">Refuser</button>
                         </div>)}
                 </div>
