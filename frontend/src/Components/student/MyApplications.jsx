bcc0325afc33e5a920bd3a7ce2641348c96798e1
SamuelGignac <samuelgignacwork@gmail.com>
2023-10-02 17:22:42 -0400

    listing my applications in front-end yay

--- frontend/src/Components/student/MyApplications.jsx
+++ frontend/src/Components/student/MyApplications.jsx
@@ -1,35 +1,48 @@
-import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../../App";
+import JobOffer from "../../model/JobOffer";
+import ShortJobOffer from "./ShortJobOffer";
 
-function MyApplications({user}) {
-
-    const [myApplications, setMyApplications] = useState([])
-
+function MyApplications({ user }) {
+    const [myApplications, setMyApplications] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
-        if(!user?.isLoggedIn){
-            navigate('/');
+        if (!user?.isLoggedIn) {
+            navigate("/");
         }
 
         async function fetchMyApplications() {
-            // TODO : waiting for backend
+            try {
+                const response = await axiosInstance.get(`/student/appliedJobOffer/${user.id}`);
+                const jobOffers = response.data.map((jobOfferData) => {
+                    const newJobOffer = new JobOffer();
+                    newJobOffer.init(jobOfferData);
+                    return newJobOffer;
+                });
+                setMyApplications(jobOffers);
+            } catch (error) {
+                console.log("Fetch error: " + error);
+            }
         }
 
-        fetchMyApplications()
-
+        fetchMyApplications();
     }, [user, navigate]);
 
-    return(
+    return (
         <div>
-            {myApplications.length === 0 ?
+            {myApplications.length === 0 ? (
                 <p>Vous n'avez pas d'applications.</p>
-                :
-                <p></p> // TODO : render applications
-            }
+            ) : (
+                <ul>
+                    {myApplications.map((offer, index) => (
+                        <ShortJobOffer jobOffer={offer} key={offer.id}/>
+                    ))}
+                </ul>
+            )}
         </div>
-    )
-
+    );
 }
 
-export default MyApplications
+export default MyApplications;
