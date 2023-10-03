05a7ec7040a01538ed0b8aba896616456ba54afe
2055619 <2055619@claurendeau.qc.ca>
2023-10-03 16:27:42 -0400

    EQ3-126 Employer ne voit pu le matricule

--- frontend/src/Components/employer/ShortStudentInfo.jsx
+++ frontend/src/Components/employer/ShortStudentInfo.jsx
@@ -4,8 +4,7 @@ const ShortStudentInfo = ({ student }) => {
     return (
         <div className="m-2 p-2 bg-white border rounded border-ose d-flex">
             <div className="col-12 col-lg-6">
-                <h3 className="text-dark fw-light">{student.firstName + " " + student.lastName + " - " + student.matricule}</h3>
-                <p className="text-dark fw-light">{student.email}</p>
+                <h3 className="text-dark fw-light">{student.firstName + " " + student.lastName + " - " + student.email}</h3>
             </div>
         </div>
     )
