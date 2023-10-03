d2e4d69c3bb4d06842a75a47435529d1d6a6971d
Zakaria <1@zaka.se>
2023-10-03 18:56:57 -0400

    EQ3-83

--- frontend/src/Components/contract/create-contract.jsx
+++ frontend/src/Components/contract/create-contract.jsx
@@ -3,13 +3,13 @@ import {toast} from "react-toastify";
 import {useTranslation} from "react-i18next";
 
 const createContract = (addContract) => {
-	const t = useTranslation();
+	const { t } = useTranslation();
 
 	const postContract = ({contract}) => {
 		axiosInstance
 			.post("manager/contract/create", contract)
 			.then((res) => {
-				toast.success(t("contract_created_successfully"));
+				toast.success(t("contract_created_successfully"))
 				addContract(res.data);
 			})
 			.catch((err) => {
