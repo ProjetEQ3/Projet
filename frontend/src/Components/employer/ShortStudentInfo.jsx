d21778230936b9859dd8b5b43d02b46435859da1
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-10-04 09:04:36 -0400

    Fix preview PDF

--- frontend/src/Components/employer/ShortStudentInfo.jsx
+++ frontend/src/Components/employer/ShortStudentInfo.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import {useTranslation} from "react-i18next";
 import {toast} from "react-toastify";
+import PDFPreview from "../util/PDFPreview";
+import CvFile from "../../model/CvFile";
 
 const ShortStudentInfo = ({ student }) => {
     const {t} = useTranslation();
+    const [isDisplay, setIsDisplay] = useState(false);
 
     const handleAccept = (e) => {
         e.preventDefault();
@@ -12,19 +15,32 @@ const ShortStudentInfo = ({ student }) => {
 
     const handleDecline = (e) => {
         e.preventDefault();
-
     }
 
+    const handlePreview = (e) => {
+        e.preventDefault();
+        setIsDisplay(!isDisplay);
+    }
+
+
     return (
-        <div className="m-2 p-2 bg-white border rounded border-ose d-flex">
-            <div className="col-12 col-lg-6">
-                <h3 className="text-dark fw-light">{student.firstName + " " + student.lastName + " - " + student.email}</h3>
+        <>
+            <div onClick={handlePreview} className="m-2 p-2 bg-white border rounded border-ose d-flex">
+                <div className="col-12 col-lg-6">
+                    <h3 className="text-dark fw-light">{student.firstName + " " + student.lastName + " - " + student.email}</h3>
+                </div>
+                <div className="col-12 col-lg-6">
+                    <button type="button" onClick={handleAccept} className="btn btn-success mx-2">{t('accept')}</button>
+                    <button type="button" onClick={handleDecline} className="btn btn-danger">{t('refuse')}</button>
+                </div>
             </div>
-            <div className="col-12 col-lg-6">
-                <button type="button" onClick={handleAccept} className="btn btn-success mx-2">{t('accept')}</button>
-                <button type="button" onClick={handleDecline} className="btn btn-danger">{t('refuse')}</button>
-            </div>
-        </div>
+            {
+                isDisplay ?
+                    <div className="col-12">
+                        <PDFPreview file={CvFile.readBytes(student.cvFile.fileData)}/>
+                    </div> : null
+            }
+        </>
     )
 }
 export default ShortStudentInfo;
