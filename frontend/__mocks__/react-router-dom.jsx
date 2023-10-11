775f327c881a32d317d5ecc08a66e31b2fe6e673
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-10-11 14:24:04 -0400

    Manager folder

--- frontend/__mocks__/react-router-dom.jsx
+++
@@ -1,7 +0,0 @@
-const reactRouterDom = jest.genMockFromModule('react-router-dom');
-
-reactRouterDom.useNavigate = () => jest.fn();
-
-module.exports = {
-    ...reactRouterDom,
-}
--- frontend/src/test/auth/LoginForm.test.jsx
+++ frontend/src/test/auth/LoginForm.test.jsx
@@ -1,39 +1,70 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import LoginForm from '../../Components/auth/LoginForm';
+import {MemoryRouter} from "react-router";
 
 describe('LoginForm Component', () => {
     it('should render the GlucOSE title', () => {
-        render(<LoginForm />);
+        render(
+            <MemoryRouter>
+                <LoginForm />
+            </MemoryRouter>
+        );
         const titleElement = screen.getByText('GlucOSE');
         expect(titleElement).toBeInTheDocument();
     });
 
     it('should display email input field', () => {
-        render(<LoginForm />);
-        const emailInput = screen.getByPlaceholderText('Email');
+        render(
+            <MemoryRouter>
+                <LoginForm />
+            </MemoryRouter>
+        );
+        const emailInput = screen.getByPlaceholderText('placeHolderEmail');
         expect(emailInput).toBeInTheDocument();
     });
 
     it('should display password input field', () => {
-        render(<LoginForm />);
-        const passwordInput = screen.getByPlaceholderText('Password'); // Replace with your actual placeholder text
+        render(
+            <MemoryRouter>
+                <LoginForm />
+            </MemoryRouter>
+        );
+        const passwordInput = screen.getByPlaceholderText('placeHolderPassword'); // Replace with your actual placeholder text
         expect(passwordInput).toBeInTheDocument();
     });
 
     it('should display the login button', () => {
-        render(<LoginForm />);
-        const loginButton = screen.getByText('Login'); // Replace with your actual button text
+        render(
+            <MemoryRouter>
+                <LoginForm />
+            </MemoryRouter>
+        );
+        const loginButton = screen.getByText('loginSubmit'); // Replace with your actual button text
         expect(loginButton).toBeInTheDocument();
     });
 
-    it('should display error messages', () => {
-        render(<LoginForm />);
-        const emailError = screen.getByText('Email error message'); // Replace with your actual error message text
-        const passwordError = screen.getByText('Password error message'); // Replace with your actual error message text
+    it('should display error messages on invalid form submission', () => {
+        render(
+            <MemoryRouter>
+                <LoginForm />
+            </MemoryRouter>
+        );
+
+        // Find the form elements and submit button
+        const emailInput = screen.getByPlaceholderText('placeHolderEmail');
+        const passwordInput = screen.getByPlaceholderText('placeHolderPassword');
+        const loginButton = screen.getByText('loginSubmit');
+
+        // Simulate an invalid form submission
+        fireEvent.change(emailInput, { target: { value: 'invalidEmail' } });
+        fireEvent.change(passwordInput, { target: { value: 'invalidPassword' } });
+        fireEvent.click(loginButton);
+
+        // Check for error messages
+        const emailError = screen.getByText('wrongEmail');
+        const passwordError = screen.getByText('wrongPassword');
         expect(emailError).toBeInTheDocument();
         expect(passwordError).toBeInTheDocument();
     });
 });
-
-// You can add more test cases for other elements and interactions as needed.
