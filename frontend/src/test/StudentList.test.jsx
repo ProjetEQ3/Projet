3cc83dc5597806bbf6b90547886fb15882773f6d
SamuelGignac <samuelgignacwork@gmail.com>
2023-10-11 12:03:59 -0400

    StudentListTest

---
+++ frontend/src/test/StudentList.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import StudentList from '../Components/employer/StudentList';
+
+describe('StudentList', () => {
+
+    const mockOffer = {
+        students: [
+            { id: 1, name: 'John Doe' },
+            { id: 2, name: 'Jane Smith' }
+        ]
+    };
+
+    it('renders without crashing', () => {
+        render(<StudentList offer={mockOffer} />);
+    });
+
+    it('renders correct number of ShortStudentInfo', () => {
+        const { getAllByTestId } = render(<StudentList offer={mockOffer} />);
+        const studentInfoComponents = getAllByTestId('short-student-info');
+        expect(studentInfoComponents).toHaveLength(mockOffer.students.length);
+    });
+
+});
