82fd4066c14ba72d5b7d02d8133a64609ed0f3d0
2055619 <2055619@claurendeau.qc.ca>
2023-10-12 21:02:30 -0400

    180-181 Controllers tests

--- src/test/java/cal/projeteq3/glucose/controller/UserControllerTest.java
+++ src/test/java/cal/projeteq3/glucose/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package cal.projeteq3.glucose.controller;
 
 import cal.projeteq3.glucose.config.SecurityConfiguration;
 import cal.projeteq3.glucose.dto.auth.LoginDTO;
+import cal.projeteq3.glucose.dto.user.UserDTO;
+import cal.projeteq3.glucose.model.user.Manager;
 import cal.projeteq3.glucose.model.user.Student;
 import cal.projeteq3.glucose.repository.UserRepository;
 import cal.projeteq3.glucose.security.JwtAuthenticationEntryPoint;
@@ -21,6 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 ;import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @SpringJUnitConfig(classes = {UserController.class, UserService.class, CustomExceptionHandler.class,
@@ -34,6 +37,7 @@ class UserControllerTest {
     private UserService userService;
     @MockBean
     private UserRepository userRepository;
+    private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsb3Vpc0BtaWNoYXVkLmNvbSIsImlhdCI6MTY5NzE1ODQyOSwiZXhwIjoxNjk3MjQ0ODI5LCJhdXRob3JpdGllcyI6W3siYXV0aG9yaXR5IjoiU1RVREVOVCJ9XX0._SRa0D-XI3IV92vcOrKu1X-5TMjeczUuGsslYyU3-zo";
 
     @Test
     void Login_Valid() throws Exception {
@@ -53,11 +57,12 @@ class UserControllerTest {
 
 //        Act
         mockMvc.perform(MockMvcRequestBuilders
-                .post("/user/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(content))
+                        .post("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
                 .andExpect(MockMvcResultMatchers.status().isAccepted());
     }
+
 
     @Test
     void Login_InvalidPassword() throws Exception {
@@ -81,6 +86,42 @@ class UserControllerTest {
 //        Act
         mockMvc.perform(MockMvcRequestBuilders.post("/user/login"))
                 .andExpect(MockMvcResultMatchers.status().is4xxClientError());
+    }
+
+    @Test
+    void getMe_ValidUser() throws Exception {
+        // Arrange
+        Student student = Student.builder()
+                .firstName("Jane")
+                .lastName("Doe")
+                .email("janedoe@exemple.com")
+                .password("Test1234")
+                .build();
+        when(userService.getMe(token)).thenReturn(new UserDTO(student));
+        when(userRepository.findUserByCredentialsEmail(anyString())).thenReturn(Optional.of(student));
+
+        // Act and Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/me")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isAccepted())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("janedoe@exemple.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Jane"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.lastName").value("Doe"))
+        ;
+    }
+
+    @Test
+    void getMe_InvalidUser() throws Exception {
+        // Arrange
+        when(userService.getMe(token)).thenReturn(null);
+
+        // Act and Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/me")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 }
 
