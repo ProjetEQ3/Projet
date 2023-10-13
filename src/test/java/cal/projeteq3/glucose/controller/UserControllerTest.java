3cb5137149bce593760871430c77063886df83d6
2055619 <2055619@claurendeau.qc.ca>
2023-10-12 20:30:25 -0400

    180-181 UserControllerTest Fonctionne

--- src/test/java/cal/projeteq3/glucose/controller/UserControllerTest.java
+++ src/test/java/cal/projeteq3/glucose/controller/UserControllerTest.java
@@ -1,6 +1,11 @@
 package cal.projeteq3.glucose.controller;
 
+import cal.projeteq3.glucose.config.SecurityConfiguration;
 import cal.projeteq3.glucose.dto.auth.LoginDTO;
+import cal.projeteq3.glucose.model.user.Student;
+import cal.projeteq3.glucose.repository.UserRepository;
+import cal.projeteq3.glucose.security.JwtAuthenticationEntryPoint;
+import cal.projeteq3.glucose.security.JwtTokenProvider;
 import cal.projeteq3.glucose.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -8,14 +13,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-;
+;import java.util.Optional;
 
-@SpringJUnitConfig(classes = {UserController.class, UserService.class, CustomExceptionHandler.class})
+import static org.mockito.Mockito.when;
+
+@SpringJUnitConfig(classes = {UserController.class, UserService.class, CustomExceptionHandler.class,
+        SecurityConfiguration.class, JwtTokenProvider.class, JwtAuthenticationEntryPoint.class})
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
@@ -23,6 +32,8 @@ class UserControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private UserService userService;
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     void Login_Valid() throws Exception {
@@ -31,10 +42,18 @@ class UserControllerTest {
         loginDTO.setPassword("Test1234");
         loginDTO.setEmail("janedoe@exemple.com");
 
+        when(userService.authenticateUser(loginDTO)).thenReturn("token");
+        when(userRepository.findUserByCredentialsEmail("janedoe@exemple.com")).thenReturn(Optional.of(
+                Student.builder()
+                        .email("janedoe@exemple.com")
+                        .password("Test1234")
+                        .build()));
+
         String content = (new ObjectMapper()).writeValueAsString(loginDTO);
 
 //        Act
-        mockMvc.perform(MockMvcRequestBuilders.post("/user/login")
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/user/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(content))
                 .andExpect(MockMvcResultMatchers.status().isAccepted());
