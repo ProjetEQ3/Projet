697efa33aa55a18d1cd62ad9883d5c721f5025c5
2055619 <2055619@claurendeau.qc.ca>
2023-10-12 20:53:14 -0400

    180-181 Pas de @CORS

--- src/main/java/cal/projeteq3/glucose/controller/UserController.java
+++ src/main/java/cal/projeteq3/glucose/controller/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/user")
 public class UserController{
