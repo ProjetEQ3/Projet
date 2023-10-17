59e3803ad6d9eb3d56f0bf11f658d236def24f20
Gabriel <2164584@claurendeau.qc.ca>
2023-10-17 15:58:01 -0400

    fe commencé

--- frontend/src/Components/layout/Main.jsx
+++ frontend/src/Components/layout/Main.jsx
@@ -6,14 +6,34 @@ import ManagerPage from "../page/ManagerPage";
 import EmployerPage from "../page/EmployerPage";
 import PageNotFound from "../page/PageNotFound";
 import NewOfferForm from "../employer/NewOfferForm";
+import {useTranslation} from "react-i18next";
 
 const Main = ({user, setUser}) => {
+    const {t} = useTranslation();
     const fixMargin = {
         paddingBottom: "10em"
     }
 
+    const sessions = [
+        {id: 1, year: 2023, season: "winter"},
+        {id: 2, year: 2022, season: "summer"}
+    ]
+
+
     return (
         <main style={fixMargin} className='App-main bg-light mx-auto'>
+            <div className="row justify-content-start pt-2">
+                <h4 className="col-6 d-flex fw-light justify-content-end m-0">
+                    {t('displayedSession')}
+                </h4>
+                <select className="col-2 d-flex justify-content-start text-capitalize">
+                    {
+                        sessions.map((session) => (
+                            <option value={session.id} className="text-capitalize">{t(session.season)} {session.year}</option>
+                        ))
+                    }
+                </select>
+            </div>
             <Routes>
                 <Route
                     path="/"
