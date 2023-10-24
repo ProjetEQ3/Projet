ae58f2f5f89a405999a2bfa1c0280733c47f6701
2055619 <2055619@claurendeau.qc.ca>
2023-10-24 15:14:28 -0400

    FE Sans test

--- frontend/src/Components/student/MyApplications.jsx
+++ frontend/src/Components/student/MyApplications.jsx
@@ -7,17 +7,12 @@ import FilterObjectList from "../util/FilterObjectList";
 import {useTranslation} from "react-i18next";
 import {toast} from "react-toastify";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPenToSquare, faX} from "@fortawesome/free-solid-svg-icons";
+import {faCalendar, faClock, faPenToSquare, faX} from "@fortawesome/free-solid-svg-icons";
 
 function MyApplications({ user }) {
     const {t} = useTranslation();
     const navigate = useNavigate();
     const [myApplications, setMyApplications] = useState([]);
-
-    const [appointments, setAppointments] = useState([
-        "2023-10-23T01:00:00.0000", "2023-10-23T02:00:00.0000", "2023-10-23T03:00:00.0000"
-    ]);
-    const [chosenAppointment, setChosenAppointment] = useState(null);
 
     useEffect(() => {
         if (!user?.isLoggedIn) {
@@ -37,33 +32,8 @@ function MyApplications({ user }) {
                 toast.error(t('fetchError') + t(error.response.data.message));
             }
         }
-        fetchAppointments();
         fetchMyApplications();
     }, [user, navigate]);
-
-    const fetchAppointments = async () => {
-        //     Get the appointments of the student (setAppointments)
-    }
-
-    function handleChosenAppointment(e) {
-        e.preventDefault();
-        const selectedAppointmentValue = e.target.querySelector('input[name="appointment"]:checked').value;
-        console.log("Selected appointment: ", selectedAppointmentValue); // Change to API call
-
-        // Close the modal
-        // const myModal = document.getElementById("appointmentModal"); // Replace "myModal" with your modal's ID
-        // const modal = new bootstrap.Modal(myModal); // Initialize the modal
-        // modal.hide(); // Close the modal
-    }
-
-    function dateTimeToShortString(appointment) {
-        let result = "";
-        result += appointment.split("T")[0].split("-").reverse().join("-");
-        result += " ";
-        result += appointment.split("T")[1].split(":").slice(0, 2).join(":");
-
-        return result;
-    }
 
     return (
         <div>
@@ -78,54 +48,7 @@ function MyApplications({ user }) {
                         renderItem={(filteredJobOffers) => (
                             <div>
                                 {filteredJobOffers.map((offer, index) => (
-                                    <>
-                                        <ShortJobOffer jobOffer={offer} key={offer.id}/>
-                                        {appointments.length > 0 && (
-                                            <div>
-                                                <div className="text-end mb-2" data-bs-toggle="modal"
-                                                     data-bs-target="#appointmentModal">
-                                                    <button className="btn btn-outline-ose my-auto"
-                                                            data-testid="appointment-button">
-                                                        {t('chooseAppointment')}
-                                                        <FontAwesomeIcon icon={faPenToSquare} className="ms-2"/>
-                                                    </button>
-                                                </div>
-                                                 <div id="appointmentModal" className="modal">
-                                                    <div className="modal-dialog">
-                                                        <div className="modal-content">
-                                                            <div className="modal-header">
-                                                                <h3 className="modal-title">{t('appointment')}<br/></h3>
-                                                                <FontAwesomeIcon icon={faX} data-bs-dismiss="modal"
-                                                                                 className="danger-hover fa-lg pe-2"/>
-                                                            </div>
-                                                            <div className="modal-body">
-                                                                <form onSubmit={handleChosenAppointment}>
-                                                                    {
-                                                                        appointments.map((appointment, index) => (
-                                                                            <div className="form-check" key={index}>
-                                                                                <input className="form-check-input"
-                                                                                       type="radio"
-                                                                                       name="appointment"
-                                                                                       id={`appointment-${index}`}
-                                                                                       value={appointment}
-                                                                                       onClick={setChosenAppointment(appointment)}/>
-                                                                                <label className="form-check-label"
-                                                                                       htmlFor={`appointment-${index}`}>
-                                                                                    {dateTimeToShortString(appointment)}
-                                                                                </label>
-                                                                            </div>
-                                                                        ))
-                                                                    }
-                                                                    <button className="btn btn-success me-3 my-2" type="submit">{t('confirm')}</button>
-                                                                    <button className="btn btn-danger my-2" type="reset" data-bs-dismiss="modal">{t('cancel')}</button>
-                                                                </form>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        )}
-                                    </>
+                                    <ShortJobOffer jobOffer={offer} key={offer.id}/>
                                 ))}
                             </div>
                         )}
--- frontend/src/Components/student/ShortJobOffer.jsx
+++ frontend/src/Components/student/ShortJobOffer.jsx
@@ -1,9 +1,45 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useTranslation } from "react-i18next";
+import {toast} from "react-toastify";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCalendar, faX} from "@fortawesome/free-solid-svg-icons";
 
 const ShortJobOffer = ({ jobOffer }) => {
     const { t } = useTranslation();
     const [isHovered, setIsHovered] = useState(false);
+
+    const [appointments, setAppointments] = useState([]);
+    const [checkboxValue, setCheckboxValue] = useState(false);
+
+    useEffect(() => {
+        fetchAppointments();
+    });
+    const fetchAppointments = async () => {
+        //     Get the appointments of the student (setAppointments)
+    }
+
+    function handleChosenAppointment(e) {
+        e.preventDefault();
+        const selectedAppointmentValue = e.target.querySelector('input[name="appointment"]:checked')?.value;
+        if (selectedAppointmentValue === null || selectedAppointmentValue === undefined || selectedAppointmentValue === "" || selectedAppointmentValue === "default") {
+            toast.error(t('noAppointmentSelected'));
+            return;
+        }
+
+        console.log("Selected appointment: ", selectedAppointmentValue); // Change to API call
+
+        toast.success(t('appointmentChosen') + '\n' + dateTimeToShortString(selectedAppointmentValue));
+        setCheckboxValue(true)
+    }
+
+    function dateTimeToShortString(appointment) {
+        let result = "";
+        result += appointment.split("T")[0].split("-").reverse().join("-");
+        result += " ";
+        result += appointment.split("T")[1].split(":").slice(0, 2).join(":");
+
+        return result;
+    }
 
     const handleMouseEnter = () => {
         setIsHovered(true);
@@ -16,7 +52,7 @@ const ShortJobOffer = ({ jobOffer }) => {
         <div className={`row ${!isHovered ? "m-2" : "m-1 shadow"}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className="col-12 bg-white rounded">
                 <div className="row">
-                    <div className="col-12">
+                    <div className="col-lg-9 col-md-8 col-sm-6">
                         <h5 data-testid="job-title" className="text-dark fw-light pt-1">
                             {jobOffer.title}
                         </h5>
@@ -24,6 +60,71 @@ const ShortJobOffer = ({ jobOffer }) => {
                             {t(jobOffer.department)}
                         </p>
                     </div>
+                    {appointments.length > 0 ? (
+                        <div className="col-md-3 col-sm-4 mt-sm-4">
+                            <div className="text-end text-sm-center mb-2" data-bs-toggle="modal"
+                                 data-bs-target="#appointmentModal">
+                                <button className="btn btn-outline-ose my-auto"
+                                        data-testid="appointment-button">
+                                    {t('chooseAppointment')}
+                                    <FontAwesomeIcon icon={faCalendar} className="ms-2"/>
+                                </button>
+                            </div>
+                            <div id="appointmentModal" className="modal">
+                                <div className="modal-dialog">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h3 className="modal-title">{t('appointments')}<br/></h3>
+                                            <FontAwesomeIcon icon={faX} data-bs-dismiss="modal"
+                                                             className="danger-hover fa-lg pe-2"/>
+                                        </div>
+                                        <div className="modal-body">
+                                            <form onSubmit={handleChosenAppointment}>
+                                                {
+                                                    appointments.map((appointment, index) => (
+                                                        <div className="form-check" key={index}>
+                                                            <input className="form-check-input"
+                                                                   type="radio"
+                                                                   name="appointment"
+                                                                   id={`appointment-${index}`}
+                                                                   value={appointment}/>
+                                                            <label className="form-check-label"
+                                                                   htmlFor={`appointment-${index}`}>
+                                                                {dateTimeToShortString(appointment)}
+                                                            </label>
+                                                        </div>
+                                                    ))
+                                                }
+                                                <div className="form-check" key="default">
+                                                    <input hidden={true}
+                                                           type="radio"
+                                                           name="appointment"
+                                                           id={`appointment-default`}
+                                                           value="default"
+                                                           checked={checkboxValue}/>
+                                                    <label hidden={true}
+                                                           htmlFor={`appointment-default`}>
+                                                        default
+                                                    </label>
+                                                </div>
+                                                <button className="btn btn-success me-3 my-2" type="submit" data-bs-dismiss="modal">{t('confirm')}</button>
+                                                <button className="btn btn-danger my-2" type="reset" data-bs-dismiss="modal">{t('cancel')}</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ): (
+                        <div className="col-lg-3 col-md-4 col-sm-4 mt-sm-4">
+                            <div className="text-end text-sm-center mb-2">
+                                <button className="btn btn-outline-ose my-auto" disabled>
+                                    {t('noAppointments')}
+                                    <FontAwesomeIcon icon={faCalendar} className="ms-2"/>
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
