7db2b1c05db8634828cf879f8423a55a473426c6
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-10-24 17:54:46 -0400

    Fix

--- frontend/src/Components/employer/InterviewedStudentList.jsx
+++ frontend/src/Components/employer/InterviewedStudentList.jsx
@@ -2,13 +2,19 @@ import React, {useEffect, useState} from 'react';
 import ShortInterviewedStudentInfo from "./ShortInterviewedStudentInfo";
 import {toast} from "react-toastify";
 import {axiosInstance} from "../../App";
+import {useTranslation} from "react-i18next";
 
 const StudentList = ({user}) => {
+    const {t} = useTranslation();
     const [studentList, setStudentList] = useState([]);
 
     useEffect(() => {
         axiosInstance.get('employer/waitingStudents', {params: {employerId: user.id}})
             .then(res => {
+                if (res.data.length === 0) {
+                    toast.info(t('noStudentsConvoked'));
+                    return;
+                }
                 setStudentList(res.data);
                 let newStudentList;
                 for (let student of studentList) {
@@ -32,11 +38,14 @@ const StudentList = ({user}) => {
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    {studentList.map((student, index) => (
+                    {
+                        studentList.length === 0 ? <h3 className="text-center">{t('noStudentsConvoked')}</h3>
+                            : studentList.map((student, index) => (
                         <div key={index}>
                             <ShortInterviewedStudentInfo student={student} jobOfferTitle={student.jobTitle}/>
                         </div>
-                    ))}
+                        ))
+                    }
                 </div>
             </div>
         </div>
