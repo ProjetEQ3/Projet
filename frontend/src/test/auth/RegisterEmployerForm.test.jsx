52364cefff4ee1abe489d70115b879f004933d2e
Zakaria <1@zaka.se>
2023-10-25 21:14:51 -0400

    tests

--- frontend/src/test/auth/RegisterEmployerForm.test.jsx
+++ frontend/src/test/auth/RegisterEmployerForm.test.jsx
@@ -1,36 +1,25 @@
 import React from 'react'
-import { render, screen, fireEvent, act } from '@testing-library/react'
+import {render, screen, fireEvent, act} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { MemoryRouter } from 'react-router-dom'
-
-
-const mockNavigate = jest.fn()
-
-jest.mock('react-router-dom', () => ({
-	...jest.requireActual('react-router-dom'),
-	useNavigate: () => mockNavigate,
-}))
+import {MemoryRouter} from 'react-router-dom'
+import RegisterEmployerForm from '../../Components/auth/RegisterEmployerForm'
+import {axiosInstance} from "../../App"
 
 jest.mock('react-i18next', () => ({
 	useTranslation: () => ({
 		t: (key) => key,
 		i18n: {
 			on: jest.fn(),
-			off: jest.fn(),
-		},
-	}),
+			off: jest.fn()
+		}
+	})
 }))
 
 jest.mock('../../App', () => ({
 	axiosInstance: {
-		post: jest.fn(),
-	},
+		post: jest.fn()
+	}
 }))
-
-import RegisterEmployerForm from '../../Components/auth/RegisterEmployerForm'
-import { axiosInstance } from "../../App"
-
-
 
 describe('<RegisterEmployerForm />', () => {
 	beforeEach(() => {
@@ -54,7 +43,6 @@ describe('<RegisterEmployerForm />', () => {
 
 	it('submits the form with valid data', async () => {
 		axiosInstance.post.mockResolvedValueOnce({ data: {} })
-
 		userEvent.type(screen.getByPlaceholderText('placeHolderFirstName'), 'John')
 		userEvent.type(screen.getByPlaceholderText('placeHolderLastName'), 'Doe')
 		userEvent.type(screen.getByPlaceholderText('placeHolderEmail'), 'john.doe@example.com')
@@ -62,33 +50,21 @@ describe('<RegisterEmployerForm />', () => {
 		userEvent.type(screen.getByPlaceholderText('placeHolderOrganisationPhone'), '+1 123-456-7890')
 		userEvent.type(screen.getByPlaceholderText('placeHolderPassword'), 'Password123')
 		userEvent.type(screen.getByPlaceholderText('placeHolderPasswordConfirm'), 'Password123')
-
-		await act(async () => {
-			fireEvent.click(screen.getByText(/registerSubmit/i))
-		})
-
+		await act(async () => {fireEvent.click(screen.getByText(/registerSubmit/i))})
 		expect(axiosInstance.post).toHaveBeenCalled()
 	})
 
 	it('displays validation errors for invalid data', async () => {
-		await act(async () => {
-			fireEvent.click(screen.getByText(/registerSubmit/i))
-		})
-
+		await act(async () => {fireEvent.click(screen.getByText(/registerSubmit/i))})
 		expect(screen.getByText('firstNameRequired')).toBeInTheDocument()
 		expect(screen.getByText('lastNameRequired')).toBeInTheDocument()
 		expect(screen.getByText('emailRequired')).toBeInTheDocument()
 		expect(screen.getByText('organisationNameRequired')).toBeInTheDocument()
 		expect(screen.getByText('organisationPhoneRequired')).toBeInTheDocument()
 		expect(screen.getByText('passwordRequired')).toBeInTheDocument()
-
 		userEvent.type(screen.getByPlaceholderText('placeHolderPassword'), 'Password123')
 		userEvent.type(screen.getByPlaceholderText('placeHolderPasswordConfirm'), 'Password456')
-
-		await act(async () => {
-			fireEvent.click(screen.getByText(/registerSubmit/i))
-		})
-
+		await act(async () => {fireEvent.click(screen.getByText(/registerSubmit/i))})
 		expect(screen.getByText('passwordConfirmInvalid')).toBeInTheDocument()
 	})
 })
