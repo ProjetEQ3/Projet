21b9fa2922142ed53539039d57abd0a721e2f52e
SamuelGignac <samuelgignacwork@gmail.com>
2023-10-31 17:11:57 -0400

    更多测试

--- frontend/src/Components/user/ShortContract.test.jsx
+++ frontend/src/Components/user/ShortContract.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import ShortContract from "../../Components/user/ShortContract";
+import ShortContract from "./ShortContract";
 
 describe('ShortContract', () => {
     const contractMock = {
