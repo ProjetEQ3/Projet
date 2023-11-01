1bbe9c4e510159437ecaa40a81aa0e1f5b5d4959
2055619 <2055619@claurendeau.qc.ca>
2023-10-31 23:15:40 -0400

    Petit ajout

--- frontend/src/Components/user/ContractList.jsx
+++ frontend/src/Components/user/ContractList.jsx
@@ -1,17 +1,23 @@
 import React, {useEffect} from 'react';
 import ShortContract from "./ShortContract";
 import { axiosInstance } from '../../App';
+import {useTranslation} from "react-i18next";
 
 const ContractList = ({ contracts, user }) => {
+    const {t} = useTranslation();
     return (
         <div className="container" data-testid="contract-list">
             <div className="row">
                 <div className="col-12">
-                    {contracts.map((contract, index) => (
+                    {contracts > 0 ? contracts.map((contract, index) => (
                         <div key={index}>
                             <ShortContract contract={contracts[index]} user={user} />
                         </div>
-                    ))}
+                    )) : (
+                        <div className="text-center">
+                            <h4>{t('noContracts')}</h4>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
