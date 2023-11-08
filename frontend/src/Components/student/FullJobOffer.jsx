13347e45d5451e5906aeda976bec078ca9207ad4
Chouaki Bendahmane <2211552@cleaurendeau.qc.ca>
2023-11-08 17:32:15 -0500

    Modal fixedÃ

--- frontend/src/Components/student/FullJobOffer.jsx
+++ frontend/src/Components/student/FullJobOffer.jsx
@@ -18,13 +18,9 @@ const FullJobOffer = ({user, jobOffer, updatedOffer}) => {
         setIsApplying(false)
     }, [jobOffer])
 
-    const applyForJobOffer = (jobOfferID) => {
-        if (!user?.id) {
-            setShowModal(false)
-            return
-        }
+    const applyForJobOffer = (jobOfferId) => {
         axiosInstance
-            .post(`/student/applyJobOffer/${user.id}/${jobOfferID}`, {coverLetter: coverLetter})
+            .post(`/student/applyJobOffer/${user.id}/${jobOfferId}`, {coverLetter: coverLetter})
             .then((response) => {
                 updatedOffer(response.data)
                 toast.success(t('appliedJobOffer'))
@@ -38,117 +34,62 @@ const FullJobOffer = ({user, jobOffer, updatedOffer}) => {
             })
     }
 
-    const handleApplyClick = () => {
-        if (!user?.id) {
-            toast.error(t('loginRequired'))
-            return
-        }
-        setShowModal(true)
-    }
-
     const handleCoverLetterChange = (e) => {
         setCoverLetter(e.target.value)
     }
 
     return (
-        <div className="row my-2">
-            <div className="col-12 bg-white rounded py-3">
-                <div className="row justify-content-between">
-                    <div className="col-9">
-                        <h2 className="text-dark fw-light pt-1">{jobOffer.title}</h2>
+        <>
+            <div className="row my-2">
+                <div className="col-12 bg-white rounded py-3">
+                    <div className="row justify-content-between">
+                        <div className="col-9">
+                            <h2 className="text-dark fw-light pt-1">{jobOffer.title}</h2>
+                        </div>
+                        <div className="col-3 my-auto text-center">
+                                <button data-testid="apply-button" className={"btn btn-outline-ose"}
+                                        data-bs-toggle="modal" data-bs-target="#apply">{t('apply')}</button>
+                        </div>
                     </div>
-                    <div className="col-3 my-auto text-center">
-                        {!isApplying ? (
-                            <button data-testid="apply-button" className={"btn btn-outline-ose"}
-                                    onClick={handleApplyClick}>
-                                {t('apply')}
-                            </button>
-                        ) : (
-                            <div>
-                                <textarea
-                                    value={coverLetter}
-                                    onChange={handleCoverLetterChange}
-                                    placeholder={t('coverLetterPlaceholder')}
-                                    className="form-control mb-2"
-                                />
-                                <button className="btn btn-outline-ose"
-                                    onClick={() => applyForJobOffer(jobOffer.id)}>
-                                    {t('apply')}
-                                </button>
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12">
-                        <h5 className="text-dark fw-light mb-3">{t(jobOffer.department)}</h5>
-                        <h5 className="text-dark fw-light mb-3">{jobOffer.location}</h5>
-                        <h6 className="text-dark fw-light mb-3">{t('startDate') + jobOffer.startDate}</h6>
-                        <h6 className="text-dark fw-light mb-3">{t('duration') + jobOffer.duration + t('week')}</h6>
-                        <p className="fst-italic fw-light text-dark">
-                            {t('estimateEndDate')} {new Date(
-                            new Date(jobOffer.startDate).getTime() + jobOffer.duration * 7 * 24 * 60 * 60 * 1000
-                        ).toISOString().split('T')[0]}
-                        </p>
-                        <h6 className="text-dark fw-light mb-3">{jobOffer.salary}$/h</h6>
-                        <h6 className="text-dark fw-light mb-3">{jobOffer.hoursPerWeek}h/{t('week')}</h6>
-                        <p className="text-dark fw-light mb-3">{jobOffer.description}</p>
+                    <div className="row">
+                        <div className="col-12">
+                            <h5 className="text-dark fw-light mb-3">{t(jobOffer.department)}</h5>
+                            <h5 className="text-dark fw-light mb-3">{jobOffer.location}</h5>
+                            <h6 className="text-dark fw-light mb-3">{t('startDate') + jobOffer.startDate}</h6>
+                            <h6 className="text-dark fw-light mb-3">{t('duration') + jobOffer.duration + t('week')}</h6>
+                            <p className="fst-italic fw-light text-dark">
+                                {t('estimateEndDate')} {new Date(
+                                new Date(jobOffer.startDate).getTime() + jobOffer.duration * 7 * 24 * 60 * 60 * 1000
+                            ).toISOString().split('T')[0]}
+                            </p>
+                            <h6 className="text-dark fw-light mb-3">{jobOffer.salary}$/h</h6>
+                            <h6 className="text-dark fw-light mb-3">{jobOffer.hoursPerWeek}h/{t('week')}</h6>
+                            <p className="text-dark fw-light mb-3">{jobOffer.description}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div className={`modal ${showModal ? 'show' : ''}`} tabIndex="-1" role="dialog">
-                <div className="modal-dialog" role="document">
+            <div id="apply" className={"modal modal-lg"}>
+                <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h3 className="modal-title">{t('appointments')}</h3>
-                            <FontAwesomeIcon icon={faX} data-bs-dismiss="modal"
-                                             className="danger-hover fa-lg pe-2"/>
+                            <h3 className="modal-title">{t('apply')}</h3>
+                            <FontAwesomeIcon icon={faTimes} data-bs-dismiss="modal" className="danger-hover fa-lg pe-2"/>
                         </div>
                         <div className="modal-body">
-                            <form onSubmit={() => applyForJobOffer(jobOffer.id)}>
-                                <textarea></textarea>
-                                <button className="btn btn-success me-3 my-2" type="submit" data-dismiss="modal">
-                                    {t('confirm')}
-                                </button>
-                                <button className="btn btn-danger my-2" type="reset" data-dismiss="modal">
-                                    {t('cancel')}
-                                </button>
+                            <form>
+                                <textarea rows="15" className={"form-control col-12"} value={coverLetter} onChange={handleCoverLetterChange} placeholder={t('coverLetterPlaceholder')}></textarea>
                             </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-danger" type="reset" data-bs-dismiss="modal">{t('cancel')}</button>
+                            <button className="btn btn-outline-ose" onClick={() => applyForJobOffer(jobOffer.id)} type="submit">{t('apply')}</button>
                         </div>
                     </div>
                 </div>
             </div>
+        </>
 
-            {/*{*/}
-            {/*    showModal &&*/}
-            {/*    <div className={`custom-modal show`}>*/}
-            {/*        <div className="custom-modal-content">*/}
-            {/*            <div className="custom-modal-header">*/}
-            {/*                <h5>{t('apply')}</h5>*/}
-            {/*                <button className="btn-close" onClick={() => setShowModal(false)}></button>*/}
-            {/*            </div>*/}
-            {/*            <div className="custom-modal-body">*/}
-            {/*            <textarea*/}
-            {/*                value={coverLetter}*/}
-            {/*                onChange={handleCoverLetterChange}*/}
-            {/*                placeholder={t('coverLetterPlaceholder')}*/}
-            {/*                className="form-control mb-2"*/}
-            {/*                style={{height: "500px"}}*/}
-            {/*            />*/}
-            {/*            </div>*/}
-            {/*            <div className="custom-modal-footer">*/}
-            {/*                <button className="btn btn-danger" onClick={() => setShowModal(false)}>*/}
-            {/*                    {t('close')}*/}
-            {/*                </button>*/}
-            {/*                <button className="btn btn-outline-ose" onClick={applyForJobOffer}>*/}
-            {/*                    {t('apply')}*/}
-            {/*                </button>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*}*/}
-        </div>
     )
 }
 
