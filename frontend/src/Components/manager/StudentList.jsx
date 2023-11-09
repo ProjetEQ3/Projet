3cc9e8f0665d38ba5dc3ff707c1951efdd9a2ac4
2055619 <2055619@claurendeau.qc.ca>
2023-11-08 20:16:16 -0500

    Ajout filtre Student List

--- frontend/src/Components/manager/StudentList.jsx
+++ frontend/src/Components/manager/StudentList.jsx
@@ -1,9 +1,28 @@
 import React, {useState} from "react";
 import {useTranslation} from "react-i18next";
 import ShortStudent from "./ShortStudent";
+import ShortCv from "./ShortCv";
+import FilterObjectList from "../util/FilterObjectList";
 
 const StudentList = ({students}) => {
     const {t} = useTranslation();
+
+    const renderFilteredStudents = (filteredStudents) => {
+        return (
+            <div className="col-12">
+                {
+                    filteredStudents.length !== 0 ?
+                        filteredStudents.map((student, index) => (
+                            <div key={index}>
+                                <ShortStudent student={student} index={index}/>
+                            </div>)) :
+                        <div className="col-12 text-center">
+                            <h4 className="text-dark fw-light">{t('noStudent')}</h4>
+                        </div>
+                }
+            </div>
+        )
+    }
 
     return (
         <div className="row">
@@ -11,12 +30,11 @@ const StudentList = ({students}) => {
                 <h3 className="text-dark fw-light my-5">{t('studentList')}</h3>
                 <div className="row justify-content-around">
                     <div className="col-12">
-                        {
-                            students.map((student, index) => (
-                                <div key={index}>
-                                    <ShortStudent student={student} index={index}/>
-                                </div>))
-                        }
+                        <FilterObjectList
+                            items={students}
+                            attributes={['firstName:' + t('firstName'),'lastName:' + t('lastName'),'matricule:'+t('matricule')]}
+                            renderItem={renderFilteredStudents}
+                        />
                     </div>
                 </div>
             </div>
