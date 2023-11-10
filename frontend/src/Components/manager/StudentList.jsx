c99561d7b6eb22dc3195ce5701001548f4ec46ca
2055619 <2055619@claurendeau.qc.ca>
2023-11-09 19:30:25 -0500

    Modif après merge

--- frontend/src/Components/manager/StudentList.jsx
+++ frontend/src/Components/manager/StudentList.jsx
@@ -28,7 +28,14 @@ const StudentList = ({students}) => {
                 <h3 className="text-dark fw-light my-5">{t('studentList')}</h3>
                 <div className="row justify-content-around">
                     <FilterObjectList
-                        items={students.map(student => {student.studentState = 'STUDENT_'+student.studentState; return student})}
+                        items={students.map(student => {
+                            if (student.studentState === 'NO_CV') student.studentState = 'STUDENT_NO_CV';
+                            if (student.studentState === 'COMPLETE') student.studentState = 'STUDENT_COMPLETE';
+                            if (student.studentState === 'NO_JOB_APPLICATION') student.studentState = 'STUDENT_NO_JOB_APPLICATION';
+                            if (student.studentState === 'NO_APPOINTMENT') student.studentState = 'STUDENT_NO_APPOINTMENT';
+                            if (student.studentState === 'NO_CONTRACT') student.studentState = 'STUDENT_NO_CONTRACT';
+                            return student
+                        })}
                         attributes={['firstName:' + t('firstName'),'lastName:' + t('lastName'),'matricule:'+ t('matricule'), 'studentState.select:Status']}
                         renderItem={renderFilteredStudents}
                         selectOptions={{studentState: ['STUDENT_NO_CV', 'STUDENT_COMPLETE', 'STUDENT_NO_JOB_APPLICATION', 'STUDENT_NO_APPOINTMENT', 'STUDENT_NO_CONTRACT']}}
