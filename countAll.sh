880c16695b7b78d8ac4612f081a10946c8840551
2055619 <2055619@claurendeau.qc.ca>
2023-11-10 10:04:20 -0500

    Sh pour compter le nb de ligne

---
+++ countAll.sh
@@ -0,0 +1,28 @@
+
+echo "Louis: "
+echo "js: "
+./gitcount.sh 2055 js 2023-08-23
+echo "java: "
+./gitcount.sh 2055 java 2023-08-23
+echo "Gabriel: "
+echo "js: "
+./gitcount.sh 216 js 2023-08-23
+./gitcount.sh Gab js 2023-08-23
+echo "java: "
+./gitcount.sh 216 java 2023-08-23
+./gitcount.sh Gab java 2023-08-23
+echo "Chawki: "
+echo "js: "
+./gitcount.sh Chouaki js 2023-08-23
+echo "java: "
+./gitcount.sh Chouaki java 2023-08-23
+echo "Samuel: "
+echo "js: "
+./gitcount.sh Samuel js 2023-08-23
+echo "java: "
+./gitcount.sh Samuel java 2023-08-23
+echo "Zakaria: "
+echo "js: "
+./gitcount.sh Zaka js 2023-08-23
+echo "java: "
+./gitcount.sh Zaka java 2023-08-23
