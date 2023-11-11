419e276531036b247a8f009056abd0bb69441eda
2055619 <2055619@claurendeau.qc.ca>
2023-11-11 13:10:29 -0500

    update instant (Delete)

--- frontend/src/Components/page/EmployerPage.jsx
+++ frontend/src/Components/page/EmployerPage.jsx
@@ -129,13 +129,14 @@ const EmployerPage = ({user}) => {
 	}
 
 	useEffect(() => {
+		getOffers()
 		setNotifications(notifications => ({
 			...notifications,
 			stages: { ...notifications.stages, red: nbPostulations + refusedCount },
 			interviewed: { ...notifications.interviewed, red: studentList.length },
 			contract: { ...notifications.contract, red: contracts.filter(contract => contract.employerSignature === null).length, green: contracts.filter(contract => contract.complete).length },
 		}));
-	}, [nbPostulations, studentList, refusedCount, contracts]);
+	}, [nbPostulations, studentList, refusedCount, contracts, offers]);
 
 	useEffect(() => {
 		getOffers();
