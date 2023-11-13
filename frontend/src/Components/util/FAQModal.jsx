75b413ef1ff53a4b3a66ca0e391a52c3ca0552d1
Gabriel <2164584@claurendeau.qc.ca>
2023-11-13 12:11:27 -0500

    css corrigé

--- frontend/src/Components/util/FAQModal.jsx
+++ frontend/src/Components/util/FAQModal.jsx
@@ -1,32 +1,25 @@
 import React, {forwardRef, useEffect, useRef, useState} from 'react'
+import {faCircleQuestion, faX} from "@fortawesome/free-solid-svg-icons";
 import {useTranslation} from "react-i18next"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const FAQModal = forwardRef(({role}, ref) => {
 	const {t, i18n} = useTranslation()
 	const [faqs, setFaqs] = useState([])
 	const faqRef = useRef(null)
-	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	useEffect(() => {
-		if(isModalOpen){
-			faqRef.current?.scrollIntoView({behavior: 'smooth'})
-		}
 		selectFaqData()
-	}, [isModalOpen, role, i18n.language])
+	}, [role, i18n.language])
 
 	const handleCloseClick = (e) => {
 		e.stopPropagation()
-		closeModal()
 	}
 
 	const toggleFAQ = index => {
 		setFaqs(faqs.map((faq, i) => ({...faq, open: i === index ? !faq.open : false})))
 	}
-
-	const closeModal = () => setIsModalOpen(false)
-
-	const openModal = () => setIsModalOpen(true)
 
 	function selectFaqData(){
 		const faqKey = role ? `FAQ_LIST.${role.replace('ROLE_', 'FAQ_')}` : ''
@@ -40,39 +33,43 @@ const FAQModal = forwardRef(({role}, ref) => {
 	}
 
 
-	return (<>
-		<button className="btn btn-outline-ose my-3 p-1 faq-button" onClick={openModal}>{t("CONSULT_FAQ")}</button>
-		<div className={`modal fade ${isModalOpen ? 'show' : ''}`} style={{display: isModalOpen ? 'block' : 'none'}}
-		     ref={faqRef}>
-			<div className="modal-dialog modal-lg modal-dialog-centered">
-				<div className="modal-content">
-					<div className="modal-header">
-						<h5 className="modal-title">{t("FAQ_TITLE")}</h5>
-						<button type="button" className="btn-close faq-close-btn" onClick={handleCloseClick}></button>
-					</div>
-					<div className="modal-body">
-						{
-							faqs && faqs.length > 0 ?
-								faqs.map((faq, index) => (
-									<div className={`faq mb-3 ${faq.open ? 'show' : ''}`} key={index}>
-										<div className="faq-question clickable my-3 p-3 bg-ose btn-outline-ose" onClick={() => toggleFAQ(index)}>
-											{faq.QUESTION}
+	return (
+		<>
+			<button className="btn btn-outline-ose FAQ-button" data-bs-toggle="modal" data-bs-target="#faqModal">
+				{t("CONSULT_FAQ")}
+				<FontAwesomeIcon icon={faCircleQuestion} className="ms-2"/>
+			</button>
+			<div id="faqModal" className="modal fade">
+				<div className="modal-dialog modal-lg modal-dialog-centered">
+					<div className="modal-content">
+						<div className="modal-header">
+								<h3 className="modal-title">{t('FAQ_TITLE')}</h3>
+								<FontAwesomeIcon data-testid="faX" icon={faX} data-bs-dismiss="modal" className="danger-hover fa-lg pe-2"/>
+						</div>
+						<div className="modal-body">
+							{
+								faqs && faqs.length > 0 ?
+									faqs.map((faq, index) => (
+										<div className={`faq mb-3 bg-light p-0 rounded ${faq.open ? 'show' : ''}`} key={index}>
+											<div className="btn btn-outline-ose fw-light col-12 text-start clickable p-3 " onClick={() => toggleFAQ(index)}>
+												{faq.QUESTION}
+											</div>
+											<div className={`collapse mb-3 px-3 ${faq.open ? 'show' : ''}`}>
+												<p className="pt-2 pb-1 ">{faq.ANSWER}</p>
+											</div>
 										</div>
-										<div className={`faq-answer collapse mb-3 px-3 ${faq.open ? 'show' : ''}`}>
-											<p>{faq.ANSWER}</p>
-										</div>
+									))
+									:
+									<div className="faq">
+										<div className="faq-question">{t("NO_FAQ")}</div>
 									</div>
-								))
-								:
-								<div className="faq">
-									<div className="faq-question">{t("NO_FAQ")}</div>
-								</div>
-						}
+							}
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</>)
+		</>
+	)
 })
 
 export default FAQModal
