6e0480c0662933817fd64be65916d609b2455e8a
SamuelGignac <samuelgignacwork@gmail.com>
2023-11-13 20:14:00 -0500

    this is taking a while URGH

--- frontend/src/Components/manager/ShortJobOffer.jsx
+++ frontend/src/Components/manager/ShortJobOffer.jsx
@@ -2,10 +2,13 @@ import {useState} from "react"
 import FullJobOffer from "./FullJobOffer"
 import State from "../util/State"
 import {useTranslation} from "react-i18next";
+import {useDarkMode} from "../../context/DarkModeContext";
 
 const ShortJobOffer = ({ jobOffer, updateJobOfferList, index, updateJobOfferListAfterApprovalOrRefusal }) => {
     const {t} = useTranslation()
     const [isHovered, setIsHovered] = useState(false)
+
+    const { darkMode } = useDarkMode();
 
     const handleMouseEnter = () => {
         setIsHovered(true)
@@ -17,12 +20,12 @@ const ShortJobOffer = ({ jobOffer, updateJobOfferList, index, updateJobOfferList
 
     return (
         <div data-testid="shadowElement" className={`row clickable ${!isHovered? 'm-2':'m-1 shadow'}`} data-bs-toggle="modal" data-bs-target={"#fullViewModal" + index}  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <div className="col-12 bg-white rounded">
+            <div className={`col-12 ${darkMode ? 'bg-light-dark' : 'bg-white'} rounded`}>
                 <div className="row">
                     <div className="col-6">
-                        <h4 data-testid="shortTitle" className="text-dark fw-light pt-1">{jobOffer.title}</h4>
-                        <p data-testid="shortDepartment" className="text-dark fw-light mb-3">{t(jobOffer.department)}</p>
-                        <p data-testid="shortCandidate" className="text-dark fw-light float-sm-end">{t('nbOfCandidates')} <span><strong>{jobOffer.nbOfCandidates}</strong></span></p>
+                        <h4 data-testid="shortTitle" className={`${darkMode ? 'text-light' : 'text-dark'} fw-light pt-1`}>{jobOffer.title}</h4>
+                        <p data-testid="shortDepartment" className={`${darkMode ? 'text-light' : 'text-dark'} fw-light mb-3`}>{t(jobOffer.department)}</p>
+                        <p data-testid="shortCandidate" className={`${darkMode ? 'text-light' : 'text-dark'} fw-light float-sm-end`}>{t('nbOfCandidates')} <span><strong>{jobOffer.nbOfCandidates}</strong></span></p>
                     </div>
                     <div className="col-6 my-auto d-flex justify-content-around">
                         <div className="d-none d-lg-block my-auto col-6 text-center">
