87d226ffd635c89b4329dba7854af35e429b4a49
2055619 <2055619@claurendeau.qc.ca>
2023-11-14 08:16:07 -0500

    Désolé pour les tests

--- src/test/java/cal/projeteq3/glucose/GlucoseApplicationTests.java
+++ src/test/java/cal/projeteq3/glucose/GlucoseApplicationTests.java
@@ -20,59 +20,9 @@ import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class GlucoseApplicationTests {
-	@Mock
-	EmployerRepository employerRepository;
-	@Mock
-	StudentRepository studentRepository;
-	@Mock
-	ManagerRepository managerRepository;
-	@Mock
-	EmployerService employerService;
-	@Mock
-	StudentService studentService;
-	@Mock
-	ManagerService managerService;
-	@Mock
-	PasswordEncoder passwordEncoder;
-
-	@InjectMocks
-	GlucoseApplication glucoseApplication;
-
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
-	}
-
-	@Test
-	void shouldCreateDatabase() {
-		glucoseApplication.createDatabase();
-
-		verify(employerRepository, times(1)).saveAll(anyList());
-		verify(studentRepository, times(1)).saveAll(anyList());
-		verify(managerRepository, times(1)).saveAll(anyList());
-	}
-
-	@Test
-	void shouldCreateApplication() {
-		glucoseApplication.createApplication();
-
-		verify(studentService, times(2)).addCv(anyLong(), any());
-		verify(managerService, times(1)).updateCvState(anyLong(), any(), any());
-		verify(studentService, times(2)).applyJobOffer(anyLong(), anyLong(), anyString());
-		verify(employerService, times(1)).addAppointmentByJobApplicationId(anyLong(), anySet());
-	}
-
-	@Test
-	void shouldRunApplication() {
-		glucoseApplication.run();
-
-		verify(employerRepository, times(1)).saveAll(anyList());
-		verify(studentRepository, times(1)).saveAll(anyList());
-		verify(managerRepository, times(1)).saveAll(anyList());
-		verify(studentService, times(2)).addCv(anyLong(), any());
-		verify(managerService, times(1)).updateCvState(anyLong(), any(), any());
-		verify(studentService, times(2)).applyJobOffer(anyLong(), anyLong(), anyString());
-		verify(employerService, times(1)).addAppointmentByJobApplicationId(anyLong(), anySet());
 	}
 
 	@Test
