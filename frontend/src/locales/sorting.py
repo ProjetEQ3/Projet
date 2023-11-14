a0735b5d53a34e4e0188e2b44054dbbdf6c68bd3
Gabriel <2164584@claurendeau.qc.ca>
2023-11-14 09:39:56 -0500

    before merge

--- frontend/src/locales/sorting.py
+++ frontend/src/locales/sorting.py
@@ -1,17 +1,21 @@
 import json
 
-# Charger le fichier JSON
+
+# Fichier EN
+with open('en.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+sorted_keys = sorted(data.keys())
+sorted_data = {key: data[key] for key in sorted_keys}
+with open('en.json', 'w', encoding='utf-8') as sorted_file:
+    json.dump(sorted_data, sorted_file, ensure_ascii=False, indent=2)
+
+
+#Fichier FR
 with open('fr.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
-
-# Trier les clés en ordre alphabétique
 sorted_keys = sorted(data.keys())
-
-# Créer un nouveau dictionnaire trié
 sorted_data = {key: data[key] for key in sorted_keys}
-
-# Sauvegarder le fichier JSON trié
-with open('fr.json', 'w', encoding='utf-8', indent=2) as sorted_file:
+with open('fr.json', 'w', encoding='utf-8') as sorted_file:
     json.dump(sorted_data, sorted_file, ensure_ascii=False, indent=2)
 
 print("Les clés du fichier JSON ont été triées avec succès.")
