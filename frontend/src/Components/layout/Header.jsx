aea41eba1f840b6ec27c3705ee2780e9785c5175
2055619 <2055619@claurendeau.qc.ca>
2023-11-15 09:21:12 -0500

    /simplify the code

--- frontend/src/Components/layout/Header.jsx
+++ frontend/src/Components/layout/Header.jsx
@@ -11,6 +11,36 @@ function Header({user}) {
     const {t} = useTranslation()
     const { darkMode, toggleDarkMode } = useDarkMode();
 
+    const DarkModeIcon = () => (
+        <FontAwesomeIcon onClick={toggleDarkMode}
+                         style={{ cursor: 'pointer', padding: '5px' }}
+                         className={`col-3 ${darkMode ? 'sun' : 'moon'}`}
+                         icon={darkMode ? faSun : faMoon}
+                         size="sm"/>
+    );
+
+    const UserSection = () => (
+        user?.isLoggedIn ?
+            <div className="m-3 col-lg-3 col-md-4 col-6 text-center lh-1">
+                <div className="d-md-flex justify-content-around align-items-center row">
+                    <DarkModeIcon/>
+                    <LngSelector/>
+                    <h2 className="display-6">{user.firstName + " " + user.lastName}</h2>
+                    <div className="m-0 p-0 float-end"></div>
+                </div>
+                <a className="text-light text-decoration-none h6" href="/" onClick={() => sessionStorage.removeItem('token') } >{t('disconnect')}</a>
+            </div> :
+            <div className="align-self-start d-md-flex m-3">
+                <div className="p-0 ps-5 ms-5 ms-md-0 ps-md-5 me-md-5 row">
+                    <DarkModeIcon/>
+                    <LngSelector/>
+                </div>
+                <a className="text-light text-decoration-none m-2 h6" href="/auth/login">{t('login')}</a>
+                <div className="vr"></div>
+                <a className="text-light text-decoration-none m-2 h6" href="/auth/register">{t('registerSubmit')}</a>
+            </div>
+    );
+
     return (
         <header className="App-header">
             <div className="container-fluid">
@@ -23,28 +53,7 @@ function Header({user}) {
                             </div>
                         </NavLink>
                         <p className="lead d-none d-lg-block col-lg-5 col-md-2 ms-auto m-xl-0 text-center">{t('glucoseFullAcronym')}</p>
-                        {user?.isLoggedIn ?
-                            <div className="m-3 col-lg-3 col-md-4 col-6 text-center lh-1">
-                                <div className="d-md-flex justify-content-around align-items-center row">
-                                    <FontAwesomeIcon onClick={toggleDarkMode} style={{ cursor: 'pointer', padding: '5px' }} className={`col-3 ${darkMode ? 'sun' : 'moon'}`} icon={darkMode ? faSun : faMoon} size="sm"/>
-                                    <LngSelector/>
-                                    <h2 className="display-6">{user.firstName + " " + user.lastName}</h2>
-                                    <div className="m-0 p-0 float-end">
-                                    </div>
-                                </div>
-                                <a className="text-light text-decoration-none h6" href="/"
-                                   onClick={() => sessionStorage.removeItem('token') } >{t('disconnect')}</a>
-                            </div> :
-                            <div className="align-self-start d-md-flex m-3">
-                                <div className="p-0 ps-5 ms-5 ms-md-0 ps-md-5 me-md-5 row">
-                                    <FontAwesomeIcon onClick={toggleDarkMode} style={{ cursor: 'pointer', padding: '5px' }} className={`col-3 ${darkMode ? 'sun' : 'moon'}`} icon={darkMode ? faSun : faMoon} size="sm"/>
-                                    <LngSelector/>
-                                </div>
-                                <a className="text-light text-decoration-none m-2 h6" href="/auth/login">{t('login')}</a>
-                                <div className="vr"></div>
-                                <a className="text-light text-decoration-none m-2 h6" href="/auth/register">{t('registerSubmit')}</a>
-                            </div>
-                        }
+                        <UserSection/>
                     </div>
                 </div>
             </div>
