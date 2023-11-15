83772f49fdb783f0526583b3bb77fed216c4f85b
2055619 <2055619@claurendeau.qc.ca>
2023-11-15 09:34:43 -0500

    Dark mode

--- frontend/src/Components/layout/Header.jsx
+++ frontend/src/Components/layout/Header.jsx
@@ -14,7 +14,7 @@ function Header({user}) {
     const DarkModeIcon = () => (
         <FontAwesomeIcon onClick={toggleDarkMode}
                          style={{ cursor: 'pointer', padding: '5px' }}
-                         className={`col-3 ${darkMode ? 'sun' : 'moon'}`}
+                         className={` ${darkMode ? 'sun' : 'moon'}`}
                          icon={darkMode ? faSun : faMoon}
                          size="sm"/>
     );
@@ -23,16 +23,22 @@ function Header({user}) {
         user?.isLoggedIn ?
             <div className="m-3 col-lg-3 col-md-4 col-6 text-center lh-1">
                 <div className="d-md-flex justify-content-around align-items-center row">
-                    <DarkModeIcon/>
-                    <LngSelector/>
-                    <h2 className="display-6">{user.firstName + " " + user.lastName}</h2>
+                    <h4 className="h4 fw-light text-nowrap">{user.firstName + " " + user.lastName}</h4>
                     <div className="m-0 p-0 float-end"></div>
                 </div>
-                <a className="text-light text-decoration-none h6" href="/" onClick={() => sessionStorage.removeItem('token') } >{t('disconnect')}</a>
-            </div> :
-            <div className="align-self-start d-md-flex m-3">
-                <div className="p-0 ps-5 ms-5 ms-md-0 ps-md-5 me-md-5 row">
+                <div className={"d-flex justify-content-around"}>
                     <DarkModeIcon/>
+                    <a className="fw-light text-decoration-none h6" href="/"
+                       onClick={() => sessionStorage.removeItem('token') } >{t('disconnect')}</a>
+                    <LngSelector/>
+                </div>
+                </div> :
+            <div className="align-self-start d-md-flex m-3">
+                <div className="p-0 ps-5 ms-5 ms-md-0 ps-md-5 me-md-5 d-flex">
+                    <div className={"mt-2"}>
+                        <DarkModeIcon/>
+                    </div>
+                    <div className={"m-2"}></div>
                     <LngSelector/>
                 </div>
                 <a className="text-light text-decoration-none m-2 h6" href="/auth/login">{t('login')}</a>
