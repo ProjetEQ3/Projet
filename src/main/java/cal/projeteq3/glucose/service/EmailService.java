faf70da9fed7cc8f85475f9a9fec43c2e8f505a4
2055619 <2055619@claurendeau.qc.ca>
2023-11-27 11:34:24 -0500

    Unused API de Zaka

--- src/main/java/cal/projeteq3/glucose/service/EmailService.java
+++ src/main/java/cal/projeteq3/glucose/service/EmailService.java
@@ -3,18 +3,12 @@ package cal.projeteq3.glucose.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -40,42 +34,6 @@ public class EmailService {
 			System.out.println(e.getMessage());
 		}
     }
-
-//	private final String apiKey = "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";
-//	private final String uri = "https://api.sendinblue.com/v3/smtp/email";
-//	private final String fromEmail = "1@zaka.se";
-//	private final String fromName = "GlucOSE Notification";
-//	private final HttpClient client = HttpClient.newHttpClient();
-//
-//	public void sendEmail(String toEmail, String toName, String subject, String content){
-//		HttpRequest request = HttpRequest
-//				.newBuilder()
-//				.uri(URI.create(uri))
-//				.header("Accept", "application/json")
-//				.header("Content-Type", "application/json")
-//				.header("api-key", apiKey)
-//				.POST(HttpRequest.BodyPublishers.ofString(getRequestBody(toEmail, toName, subject, content)))
-//				.build();
-//		try{
-//			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//			System.out.println("CODE: " + response.statusCode() + " TO: " + toEmail);
-//		}catch(Exception e){
-//			System.out.println("Exception: " + e.getMessage());
-//		}
-//	}
-//
-//	private String getRequestBody(String toEmail, String toName, String subject, String content){
-//		String htmlContent = getString(subject, content);
-//		return String.format(
-//				"{" +
-//						"\"sender\":{\"name\":\"%s\",\"email\":\"%s\"}," +
-//						"\"to\":[{\"email\":\"%s\",\"name\":\"%s\"}]," +
-//						"\"subject\":\"%s\"," +
-//						"\"htmlContent\":\"%s\"," +
-//						"\"textContent\":\"%s\"}",
-//				fromName, fromEmail, toEmail, toName, subject, htmlContent, content
-//		);
-//	}
 
 	private static String getString(String subject, String content) {
 		return "<html>" +
